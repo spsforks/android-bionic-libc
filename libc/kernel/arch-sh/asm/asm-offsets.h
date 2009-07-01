d3ed7746d19044d35508e4bf65ffcc3e650e304a
Tony Sim <sim.chinyeow@renesas.com>
2009-07-01 15:48:00 +0900

    added headers for SuperH which automatically generate by update_all.py

---
+++ libc/kernel/arch-sh/asm/asm-offsets.h
@@ -0,0 +1,22 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef __ASM_OFFSETS_H__
+#define __ASM_OFFSETS_H__
+
+#define TI_TASK 0  
+#define TI_EXEC_DOMAIN 4  
+#define TI_FLAGS 8  
+#define TI_CPU 12  
+#define TI_PRE_COUNT 16  
+#define TI_RESTART_BLOCK 24  
+
+#endif
