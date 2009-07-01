41483d09d6ffd175f254d56746c1dfa4adf435dc
Tony Sim <sim.chinyeow@renesas.com>
2009-07-01 15:48:00 +0900

    added headers for SuperH which automatically generate by update_all.py

---
+++ libc/kernel/arch-sh/asm/machtypes.h
@@ -0,0 +1,91 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef __ASM_SH_MACHTYPES_H
+#define __ASM_SH_MACHTYPES_H
+
+#define MACH_SE 0
+#define MACH_HIGHLANDER 0
+#define MACH_RTS7751R2D 0
+#define MACH_HD64461 0
+#define MACH_HD64465 0
+#define MACH_7206SE 0
+#define MACH_7343SE 0
+#define MACH_7619SE 0
+#define MACH_7721SE 0
+#define MACH_7722SE 0
+#define MACH_7751SE 0
+#define MACH_7780SE 0
+#define MACH_7751SYSTEMH 0
+#define MACH_HP6XX 0
+#define MACH_DREAMCAST 0
+#define MACH_SNAPGEAR 0
+#define MACH_EDOSK7705 0
+#define MACH_SH4202_MICRODEV 0
+#define MACH_SH03 0
+#define MACH_LANDISK 0
+#define MACH_R7780RP 0
+#define MACH_R7780MP 0
+#define MACH_R7785RP 0
+#define MACH_TITAN 0
+#define MACH_SHMIN 0
+#define MACH_LBOXRE2 0
+#define MACH_X3PROTO 0
+#define MACH_MAGICPANELR2 0
+#define MACH_R2D_PLUS 0
+#define MACH_R2D_1 0
+#define MACH_CAYMAN 0
+#define MACH_SDK7780 0
+#define MACH_MIGOR 0
+#define MACH_RSK7203 0
+#define MACH_AP325RXA 1
+#define MACH_SH7763RDP 0
+#define MACH_SH7785LCR 0
+
+#define mach_is_se() (MACH_SE)
+#define mach_is_highlander() (MACH_HIGHLANDER)
+#define mach_is_rts7751r2d() (MACH_RTS7751R2D)
+#define mach_is_hd64461() (MACH_HD64461)
+#define mach_is_hd64465() (MACH_HD64465)
+#define mach_is_7206se() (MACH_7206SE)
+#define mach_is_7343se() (MACH_7343SE)
+#define mach_is_7619se() (MACH_7619SE)
+#define mach_is_7721se() (MACH_7721SE)
+#define mach_is_7722se() (MACH_7722SE)
+#define mach_is_7751se() (MACH_7751SE)
+#define mach_is_7780se() (MACH_7780SE)
+#define mach_is_7751systemh() (MACH_7751SYSTEMH)
+#define mach_is_hp6xx() (MACH_HP6XX)
+#define mach_is_dreamcast() (MACH_DREAMCAST)
+#define mach_is_snapgear() (MACH_SNAPGEAR)
+#define mach_is_edosk7705() (MACH_EDOSK7705)
+#define mach_is_sh4202_microdev() (MACH_SH4202_MICRODEV)
+#define mach_is_sh03() (MACH_SH03)
+#define mach_is_landisk() (MACH_LANDISK)
+#define mach_is_r7780rp() (MACH_R7780RP)
+#define mach_is_r7780mp() (MACH_R7780MP)
+#define mach_is_r7785rp() (MACH_R7785RP)
+#define mach_is_titan() (MACH_TITAN)
+#define mach_is_shmin() (MACH_SHMIN)
+#define mach_is_lboxre2() (MACH_LBOXRE2)
+#define mach_is_x3proto() (MACH_X3PROTO)
+#define mach_is_magicpanelr2() (MACH_MAGICPANELR2)
+#define mach_is_r2d_plus() (MACH_R2D_PLUS)
+#define mach_is_r2d_1() (MACH_R2D_1)
+#define mach_is_cayman() (MACH_CAYMAN)
+#define mach_is_sdk7780() (MACH_SDK7780)
+#define mach_is_migor() (MACH_MIGOR)
+#define mach_is_rsk7203() (MACH_RSK7203)
+#define mach_is_ap325rxa() (MACH_AP325RXA)
+#define mach_is_sh7763rdp() (MACH_SH7763RDP)
+#define mach_is_sh7785lcr() (MACH_SH7785LCR)
+
+#endif
---
+++ libc/kernel/arch-sh/asm/types.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ****************************************************************************
+ ****************************************************************************/
+#ifndef __ASM_SH_TYPES_H
+#define __ASM_SH_TYPES_H
+
+#include <asm-generic/int-ll64.h>
+
+#ifndef __ASSEMBLY__
+
+typedef unsigned short umode_t;
+
+#endif
+
+#endif
