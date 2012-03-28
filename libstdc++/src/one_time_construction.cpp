abe842104d2d481b35a8b7e06600ae8b1363c739
Fengwei Yin <fengwei.yin@intel.com>
2012-03-28 17:25:17 +0800

    libstdc++: Fix x86 thread-safe one-time-construction implementation.
    The root of the problem is that the existing implementation is based on the
    ARM C++ ABI, which mandates that only the lsb of the guard variable should
    be tested by compiler-generated code before calling __cxa_guard_acquire et al.
    
    The Itanium / x86 C++ ABIs define a different convention where the low-order
    _byte_ is tested instead.
    
    Meanwhile, guard variables are 32-bit aligned for ARM, and 64-bit for x86.
    
    Reference documentation:
    
        section 3.2.3 of ARM IHI 0041C (for ARM)
        section 3.3.2 of the Itanium C++ ABI specification v1.83 (for x86).
    
    This patch modifies the implementation in a way that satisfies both ABIs (and
    doesn't require changing the toolchains).
    
    Change-Id: I885e9adc7f088b9c0a78355bd752f1e6aeec9f07
    Signed-off-by: Fengwei Yin <fengwei.yin@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- libstdc++/src/one_time_construction.cpp
+++ libstdc++/src/one_time_construction.cpp
@@ -13,43 +13,76 @@
 #include <bionic_futex.h>
 #include <bionic_atomic_inline.h>
 
-extern "C" int __cxa_guard_acquire(int volatile * gv)
+// NOTE: XXX
+// If endian is supported in bionic, this define need be updated.
+#ifndef le32_to_cpu
+#define le32_to_cpu(x)    (x)
+#endif
+
+#if __arm__
+// For arm, check the LSB of 32 bit guard variable.
+typedef uint32_t _guard_t;
+#else
+// For others, check the first byte of 64bit guard variable
+typedef uint64_t _guard_t;
+#endif
+
+// if _guard_t is 64bit, it can't be used by futex directly.
+// union instead of cast could make sure gv_v and gv_t share
+// the same "first byte" for ARM vs others and LE vs BE.
+union _guard_u {
+    _guard_t    gv_m;   // _guard_t for gv memory
+    int         gv_i;   // int for futex
+};
+
+const static int ready     = le32_to_cpu(0x1);
+const static int pending   = le32_to_cpu(0x100);
+const static int waiting   = le32_to_cpu(0x10000);
+
+extern "C" int __cxa_guard_acquire(_guard_t volatile * gv)
 {
-    // 0 -> 2, return 1
-    // 2 -> 6, wait and return 0
-    // 6 untouched, wait and return 0
-    // 1 untouched, return 0
+    // 0 -> pending, return 1
+    // pending -> waiting, wait and return 0
+    // waiting: untouched, wait and return 0
+    // ready: untouched, return 0
+
+    union _guard_u *gv_u = (union _guard_u *)gv;
+
 retry:
-    if (__atomic_cmpxchg(0, 0x2, gv) == 0) {
+    if (__atomic_cmpxchg(0, pending, &gv_u->gv_i) == 0) {
         ANDROID_MEMBAR_FULL();
         return 1;
     }
-    __atomic_cmpxchg(0x2, 0x6, gv); // Indicate there is a waiter
-    __futex_wait(gv, 0x6, NULL);
+    __atomic_cmpxchg(pending, waiting, &gv_u->gv_i); // Indicate there is a waiter
+    __futex_wait(&gv_u->gv_i, waiting, NULL);
 
-    if(*gv != 1) // __cxa_guard_abort was called, let every thread try since there is no return code for this condition
+    if (gv_u->gv_i != ready) // __cxa_guard_abort was called, let every thread try since there is no return code for this condition
         goto retry;
 
     ANDROID_MEMBAR_FULL();
     return 0;
 }
 
-extern "C" void __cxa_guard_release(int volatile * gv)
+extern "C" void __cxa_guard_release(_guard_t volatile * gv)
 {
-    // 2 -> 1
-    // 6 -> 1, and wake
+    // pending -> ready
+    // waiting -> ready, and wake
+    union _guard_u *gv_u = (union _guard_u *)gv;
+
     ANDROID_MEMBAR_FULL();
-    if (__atomic_cmpxchg(0x2, 0x1, gv) == 0) {
+    if (__atomic_cmpxchg(pending, ready, &gv_u->gv_i) == 0) {
         return;
     }
 
-    *gv = 0x1;
-    __futex_wake(gv, 0x7fffffff);
+    gv_u->gv_i = ready;
+    __futex_wake(&gv_u->gv_i, 0x7fffffff);
 }
 
 extern "C" void __cxa_guard_abort(int volatile * gv)
 {
+    union _guard_u *gv_u = (union _guard_u *)gv;
+
     ANDROID_MEMBAR_FULL();
-    *gv = 0;
-    __futex_wake(gv, 0x7fffffff);
+    gv_u->gv_i = 0;
+    __futex_wake(&gv_u->gv_i, 0x7fffffff);
 }
