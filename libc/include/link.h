ad2774555572a457715b2866eb1e2ae93217f0ae
Pavel Chupin <pavel.v.chupin@intel.com>
2012-08-17 12:53:29 +0400

    Add link.h for all platforms with dl_phdr_info
    This header is used on bionic build and should be propagated into
    sysroot on toolchain rebuild. Discussion re. this header is here:
    http://gcc.gnu.org/ml/gcc-patches/2012-08/msg00936.html
    
    It is available already in mips NDK platforms:
    development/ndk/platforms/android-9/arch-mips/include/link.h
    
    Change-Id: I39ff467cdac9f448e31c11ee3e14a6200e82ab57
    Signed-off-by: Pavel Chupin <pavel.v.chupin@intel.com>

--- libc/include/link.h
+++ libc/include/link.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006 The Android Open Source Project
+ * Copyright (C) 2012 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,41 +25,41 @@
  * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
+#ifndef _LINK_H_
+#define _LINK_H_
 
 #include <sys/types.h>
-#include <linux/elf.h>
-#include <link.h>
+#include <elf.h>
 
-/* Dynamic binaries get this from the dynamic linker (system/linker), which
- * we don't pull in for static bins. We also don't have a list of so's to
- * iterate over, since there's really only a single monolithic blob of
- * code/data.
- *
- * All we need to do is to find where the executable is in memory, and grab the
- * phdr and phnum from there.
- */
+__BEGIN_DECLS
 
-/* ld provides this to us in the default link script */
-extern void *__executable_start;
-
-int
-dl_iterate_phdr(int (*cb)(struct dl_phdr_info *info, size_t size, void *data),
-                void *data)
+struct dl_phdr_info
 {
-    struct dl_phdr_info dl_info;
-    Elf32_Ehdr *ehdr = (Elf32_Ehdr *) &__executable_start;
-    Elf32_Phdr *phdr = (Elf32_Phdr *)((unsigned long)ehdr + ehdr->e_phoff);
+    Elf32_Addr dlpi_addr;
+    const char *dlpi_name;
+    const Elf32_Phdr *dlpi_phdr;
+    Elf32_Half dlpi_phnum;
+};
 
-    /* TODO: again, copied from linker.c. Find a better home for this
-     * later. */
-    if (ehdr->e_ident[EI_MAG0] != ELFMAG0) return -1;
-    if (ehdr->e_ident[EI_MAG1] != ELFMAG1) return -1;
-    if (ehdr->e_ident[EI_MAG2] != ELFMAG2) return -1;
-    if (ehdr->e_ident[EI_MAG3] != ELFMAG3) return -1;
+#ifdef __mips__
+# if _MIPS_SZPTR == 32
+#  define ElfW(type)      Elf32_##type
+# elif _MIPS_SZPTR == 64
+#  define ElfW(type)      Elf64_##type
+# endif
+#elif __x86_64__
+#  define ElfW(type)      Elf64_##type
+#else
+#  define ElfW(type)      Elf32_##type
+#endif
 
-    dl_info.dlpi_addr = 0;
-    dl_info.dlpi_name = NULL;
-    dl_info.dlpi_phdr = phdr;
-    dl_info.dlpi_phnum = ehdr->e_phnum;
-    return cb(&dl_info, sizeof (struct dl_phdr_info), data);
-}
+#ifdef __arm__
+typedef long unsigned int *_Unwind_Ptr;
+_Unwind_Ptr dl_unwind_find_exidx(_Unwind_Ptr pc, int *pcount);
+#else
+int dl_iterate_phdr(int (*cb)(struct dl_phdr_info *, size_t, void *), void *);
+#endif
+
+__END_DECLS
+
+#endif /* _LINK_H_ */
