840bf5dbd447f7a49a4a56a5d6976d2dad57d1b6
Ard Biesheuvel <ard.biesheuvel@gmail.com>
2012-08-21 14:13:50 +0200

    ARM: make CRT_LEGACY_WORKAROUND work as (supposedly) intended
    To properly support legacy ARM shared libraries, libc.so needs
    to export the symbols __dso_handle and atexit, even though
    these are now supplied by the crt startup code.
    
    This patch reshuffles the existing CRT_LEGACY_WORKAROUND
    conditionally compiled code slightly so it works as the
    original author likely intended.
    
    Change-Id: Id6c0e94dc65b7928324a5f0bad7eba6eb2f464b9
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@gmail.com>

--- libc/Android.mk
+++ libc/Android.mk
@@ -144,7 +144,6 @@ libc_common_src_files := \
 	stdio/__vsprintf_chk.c \
 	stdlib/_rand48.c \
 	stdlib/assert.c \
-	stdlib/atexit.c \
 	stdlib/atoi.c \
 	stdlib/atol.c \
 	stdlib/atoll.c \
@@ -357,6 +356,7 @@ libc_upstream_netbsd_src_files := \
 libc_static_common_src_files := \
         unistd/sysconf.c \
         bionic/__errno.c \
+        stdlib/atexit.c \
 
 # Architecture specific source files go here
 # =========================================================
@@ -584,10 +584,6 @@ libc_crt_target_cflags += \
     -I$(LOCAL_PATH)/include  \
     -DPLATFORM_SDK_VERSION=$(PLATFORM_SDK_VERSION)
 
-ifeq ($(TARGET_ARCH),arm)
-    libc_crt_target_cflags += -DCRT_LEGACY_WORKAROUND
-endif
-
 # Define some common includes
 # ========================================================
 libc_common_c_includes := \
@@ -762,9 +758,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(libc_common_src_files)
 LOCAL_CFLAGS := $(libc_common_cflags)
-ifeq ($(TARGET_ARCH),arm)
-    LOCAL_CFLAGS += -DCRT_LEGACY_WORKAROUND
-endif
 LOCAL_C_INCLUDES := $(libc_common_c_includes)
 LOCAL_MODULE := libc_common
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
@@ -847,6 +840,16 @@ LOCAL_SRC_FILES := \
 	bionic/malloc_debug_common.c \
 	bionic/pthread_debug.c \
 	bionic/libc_init_dynamic.c
+
+ifeq ($(TARGET_ARCH),arm)
+	LOCAL_NO_CRT := true
+	LOCAL_CFLAGS += -DCRT_LEGACY_WORKAROUND
+
+	LOCAL_SRC_FILES := \
+		arch-arm/bionic/crtbegin_so.c \
+		$(LOCAL_SRC_FILES) \
+		arch-arm/bionic/crtend_so.S
+endif
 
 LOCAL_MODULE:= libc
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
