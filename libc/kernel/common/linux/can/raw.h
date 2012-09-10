19b81f619371ef6d80cbea9014656db3363682b3
Raghu Gandham <raghu@mips.com>
2012-09-10 14:47:13 -0700

    Update the kernel headers by running libc/kernel/tools/update_all.py.
    Change-Id: I9da65e9d2f5bdec0d0c9eeb796ba531883124934

--- libc/kernel/common/linux/can/raw.h
+++ libc/kernel/common/linux/can/raw.h
@@ -16,29 +16,16 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef _LINUX_SECCOMP_H
-#define _LINUX_SECCOMP_H
-#include <linux/compiler.h>
-#include <linux/types.h>
+#ifndef CAN_RAW_H
+#define CAN_RAW_H
+#include <linux/can.h>
+#define SOL_CAN_RAW (SOL_CAN_BASE + CAN_RAW)
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define SECCOMP_MODE_DISABLED 0  
-#define SECCOMP_MODE_STRICT 1  
-#define SECCOMP_MODE_FILTER 2  
-#define SECCOMP_RET_KILL 0x00000000U  
+enum {
+ CAN_RAW_FILTER = 1,
+ CAN_RAW_ERR_FILTER,
+ CAN_RAW_LOOPBACK,
 /* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define SECCOMP_RET_TRAP 0x00030000U  
-#define SECCOMP_RET_ERRNO 0x00050000U  
-#define SECCOMP_RET_TRACE 0x7ff00000U  
-#define SECCOMP_RET_ALLOW 0x7fff0000U  
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define SECCOMP_RET_ACTION 0x7fff0000U
-#define SECCOMP_RET_DATA 0x0000ffffU
-struct seccomp_data {
- int nr;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- __u32 arch;
- __u64 instruction_pointer;
- __u64 args[6];
+ CAN_RAW_RECV_OWN_MSGS
 };
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #endif
