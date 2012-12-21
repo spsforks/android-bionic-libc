c01e74b9b926ac04ae176dca5ba73dd6e29fbf09
Andrew Hsieh <andrewhsieh@google.com>
2012-12-21 15:41:15 +0800

    Fix full_mips-eng checkbuild
    1. Add deifnition point of __fpsr, otherwise GCC 4.7 complains
       about '__fpsr' may be used uninitialized in this function
    2. Preclude MIPS from building no-elf-hash-table-library.so
       because .gnu.hash is incompatible with the MIPS ABI
    
    Change-Id: I0a8cd89c566eb7259f828cba28b78a84292651c6

--- tests/Android.mk
+++ tests/Android.mk
@@ -96,6 +96,8 @@ include $(BUILD_NATIVE_TEST)
 # Test library for the unit tests.
 # -----------------------------------------------------------------------------
 
+ifneq ($(TARGET_ARCH),mips)
+
 # Build no-elf-hash-table-library.so to test dlopen(3) on a library that
 # only has a GNU-style hash table.
 include $(CLEAR_VARS)
@@ -104,6 +106,8 @@ LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_SRC_FILES := empty.cpp
 LOCAL_LDFLAGS := -Wl,--hash-style=gnu
 include $(BUILD_SHARED_LIBRARY)
+
+endif
 
 # -----------------------------------------------------------------------------
 # Unit tests built against glibc.
