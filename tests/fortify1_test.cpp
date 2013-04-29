9671ce0fc98067e8debcc9753c4a3550ff93b77d
Nick Kralevich <nnk@google.com>
2013-04-29 14:07:06 -0700

    strncpy: implement _FORTIFY_SOURCE=2
    Add support for fortify source level 2 to strncpy.
    This will enable detection of more areas where strncpy
    is used inappropriately. For example, this would have detected
    bug 8727221.
    
    Move the fortify_source tests out of string_test.cpp, and
    put it into fortify1_test.cpp.
    
    Create a new fortify2_test.cpp file, which incorporates all
    the tests in fortify1_test.cpp, and adds fortify_source level
    2 specific tests.
    
    Change-Id: Ica0fba531cc7d0609e4f23b8176739b13f7f7a83

---
+++ tests/fortify1_test.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FORTIFY_TEST
+#define FORTIFY_TEST(test_name) TEST(Fortify1_DeathTest, test_name)
+#undef _FORTIFY_SOURCE
+#define _FORTIFY_SOURCE 1
+#endif
+
+#include <gtest/gtest.h>
+#include <string.h>
+
+#if __BIONIC__
+// We have to say "DeathTest" here so gtest knows to run this test (which exits)
+// in its own process.
+FORTIFY_TEST(strcpy_fortified) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  char buf[10];
+  char *orig = strdup("0123456789");
+  ASSERT_EXIT(strcpy(buf, orig), testing::KilledBySignal(SIGSEGV), "");
+  free(orig);
+}
+
+FORTIFY_TEST(strlen_fortified) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  char buf[10];
+  memcpy(buf, "0123456789", sizeof(buf));
+  ASSERT_EXIT(printf("%d", strlen(buf)), testing::KilledBySignal(SIGSEGV), "");
+}
+
+FORTIFY_TEST(strchr_fortified) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  char buf[10];
+  memcpy(buf, "0123456789", sizeof(buf));
+  ASSERT_EXIT(printf("%s", strchr(buf, 'a')), testing::KilledBySignal(SIGSEGV), "");
+}
+
+FORTIFY_TEST(strrchr_fortified) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  char buf[10];
+  memcpy(buf, "0123456789", sizeof(buf));
+  ASSERT_EXIT(printf("%s", strrchr(buf, 'a')), testing::KilledBySignal(SIGSEGV), "");
+}
+#endif
