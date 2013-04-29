3c7f77886e592876c77d6bbced54cba06f07ed59
Nick Kralevich <nnk@google.com>
2013-04-29 14:07:06 -0700

    strncpy: implement _FORTIFY_SOURCE=2
    Add support for fortify source level 2 to strncpy.
    This will enable detection of more areas where strncpy
    is used inappropriately. For example, this would have detected
    bug 8727221.
    
    Move the fortify_source tests out of string_test.cpp, and
    put it into fortify1_test.cpp.
    
    Create a new fortify2_test.cpp file, which incorporates all
    the tests in fortify1_test.cpp, and adds fortify_source level
    2 specific tests.
    
    Change-Id: Ica0fba531cc7d0609e4f23b8176739b13f7f7a83

---
+++ tests/fortify2_test.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#undef _FORTIFY_SOURCE
+#define _FORTIFY_SOURCE 2
+// We have to say "DeathTest" here so gtest knows to run this test (which exits)
+// in its own process.
+#define FORTIFY_TEST(test_name) TEST(Fortify2_DeathTest, test_name)
+
+// All fortify source level 1 tests should pass.
+#include "fortify1_test.cpp"
+
+// Start fortify level 2 tests
+
+struct foo {
+  char a[10];
+  char b[10];
+};
+
+FORTIFY_TEST(strncpy_fortified) {
+  ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+  foo myfoo;
+  int copy_amt = atoi("11");
+  ASSERT_EXIT(strncpy(myfoo.a, "01234567890", copy_amt),
+              testing::KilledBySignal(SIGSEGV), "");
+}
