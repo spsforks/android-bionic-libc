6fa97e8e753c1010d49e1769d81cd18d93297712
Serban Constantinescu <serban.constantinescu@arm.com>
2013-10-16 14:14:01 +0100

    AArch64: Fix libc build for PTHREAD_DEBUG_ENABLED
    This patch will fix the libc build when built with PTHREAD_DEBUG_ENABLED=1
    for all supported architectures, by adding missing source files.
    
    Change-Id: Ibec4512dee53909a2b4175a3d74a9ad90b4aefb3
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

--- libc/Android.mk
+++ libc/Android.mk
@@ -1029,6 +1029,8 @@ LOCAL_SRC_FILES := \
 	bionic/dlmalloc.c \
 	bionic/malloc_debug_common.cpp \
 	bionic/pthread_debug.cpp \
+	bionic/debug_stacktrace.cpp \
+	bionic/debug_mapinfo.cpp \
 	bionic/libc_init_dynamic.cpp
 
 ifeq ($(TARGET_ARCH),arm)
