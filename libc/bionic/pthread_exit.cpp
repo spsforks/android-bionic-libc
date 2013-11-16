4d8cb67463e824e61ff9652c98c92d0f090bb59c
Elliott Hughes <enh@google.com>
2013-11-15 17:40:18 -0800

    Fix pthread_join.
    Let the kernel keep pthread_internal_t::tid updated, including
    across forks and for the main thread. This then lets us fix
    pthread_join to only return after the thread has really exited.
    
    Also fix the thread attributes of the main thread so we don't
    unmap the main thread's stack (which is really owned by the
    dynamic linker and contains things like environment variables),
    which fixes crashes when joining with an exited main thread
    and also fixes problems reported publicly with accessing environment
    variables after the main thread exits (for which I've added a new
    unit test).
    
    In passing I also fixed a bug where if the clone(2) inside
    pthread_create(3) fails, we'd unmap the child's stack and TLS (which
    contains the mutex) and then try to unlock the mutex. Boom! It wasn't
    until after I'd uploaded the fix for this that I came across a new
    public bug reporting this exact failure.
    
    Bug: 8206355
    Bug: 11693195
    Bug: https://code.google.com/p/android/issues/detail?id=57421
    Bug: https://code.google.com/p/android/issues/detail?id=62392
    Change-Id: I2af9cf6e8ae510a67256ad93cad891794ed0580b

--- libc/bionic/pthread_exit.cpp
+++ libc/bionic/pthread_exit.cpp
@@ -57,7 +57,7 @@ void __pthread_cleanup_pop(__pthread_cleanup_t* c, int execute) {
   }
 }
 
-void pthread_exit(void* retval) {
+void pthread_exit(void* return_value) {
   pthread_internal_t* thread = __get_thread();
 
   // Call the cleanup handlers first.
@@ -93,7 +93,7 @@ void pthread_exit(void* retval) {
   // If the thread is detached, destroy the pthread_internal_t,
   // otherwise keep it in memory and signal any joiners.
   pthread_mutex_lock(&gThreadListLock);
-  if (thread->attr.flags & PTHREAD_ATTR_FLAG_DETACHED) {
+  if ((thread->attr.flags & PTHREAD_ATTR_FLAG_DETACHED) != 0) {
     _pthread_internal_remove_locked(thread);
   } else {
     // Make sure that the pthread_internal_t doesn't have stale pointers to a stack that
@@ -106,12 +106,7 @@ void pthread_exit(void* retval) {
 
     // Indicate that the thread has exited for joining threads.
     thread->attr.flags |= PTHREAD_ATTR_FLAG_ZOMBIE;
-    thread->return_value = retval;
-
-    // Signal the joining thread if present.
-    if (thread->attr.flags & PTHREAD_ATTR_FLAG_JOINED) {
-      pthread_cond_signal(&thread->join_cond);
-    }
+    thread->return_value = return_value;
   }
   pthread_mutex_unlock(&gThreadListLock);
 
@@ -131,6 +126,6 @@ void pthread_exit(void* retval) {
     _exit_with_stack_teardown(stack_base, stack_size, 0);
   }
 
-  /* NOTREACHED, but we told the compiler this function is noreturn, and it doesn't believe us. */
+  // NOTREACHED, but we told the compiler this function is noreturn, and it doesn't believe us.
   abort();
 }
