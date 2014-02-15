300a4ab96c3e3ca2994240ebd7c54ee704fe2066
Colin Cross <ccross@android.com>
2014-02-14 18:56:23 -0800

    bionic: fix __set_errno for arm64 syscalls that return a 64-bit value
    bionic/libc/arch-arm64/syscalls/read.S ends with:
        b.hi __set_errno
        ret
    END(read)
    
    If __set_errno returns int, it will set w0 to 0xFFFFFFFF, which means
    x0 is 0x00000000FFFFFFFF.  When interpreted as a ssize_t that is
    INT_MAX, not -1.
    
    Change __set_errno to return long, which will cause x0 to be set instead
    of w0.
    
    Change-Id: I9f9ea0f2995928d2ea240eb2ff7758ecdf0ff412

--- libc/bionic/__set_errno.cpp
+++ libc/bionic/__set_errno.cpp
@@ -32,7 +32,7 @@
 // C/C++ code should just assign 'errno' instead.
 
 // TODO: this should be __LIBC_HIDDEN__ but was exposed in <errno.h> in the NDK.
-extern "C" int __set_errno(int n) {
+extern "C" long __set_errno(int n) {
   errno = n;
   return -1;
 }
