15ace593c45ffa9b1d06384101aac2677ea8da1d
Mark Salyzyn <salyzyn@google.com>
2014-03-18 20:47:02 +0000

    Revert "logd: default user space logger"
    This reverts commit c1005aaf8ea8f9eab515a5b06b80a706faba6e43.
    
    Change-Id: I5671c7b8dd9413cee80d06c1aae219d870306c19

--- libc/Android.mk
+++ libc/Android.mk
@@ -624,7 +624,7 @@ LOCAL_SYSTEM_SHARED_LIBRARIES :=
 # Since this code is experimental it is disabled by default.
 LOCAL_CFLAGS += -DPTHREAD_DEBUG_ENABLED=false
 
-ifneq ($(TARGET_USES_LOGD),false)
+ifeq ($(TARGET_USES_LOGD),true)
 LOCAL_CFLAGS += -DTARGET_USES_LOGD
 endif
 
