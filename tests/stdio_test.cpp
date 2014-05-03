443c9dccb536c27cf2e928252ad4b332e2db70c3
Elliott Hughes <enh@google.com>
2014-05-02 18:16:32 -0700

    Disable %n in printf and vfwprintf.
    Change-Id: If190bede29e5f68a65043ddbe8e878c660933d03

--- tests/stdio_test.cpp
+++ tests/stdio_test.cpp
@@ -221,10 +221,10 @@ TEST(stdio, snprintf_ls) {
 
 TEST(stdio, snprintf_n) {
   char buf[32];
-  int i = 0;
-  EXPECT_EQ(4, snprintf(buf, sizeof(buf), "a %n b", &i));
-  EXPECT_EQ(2, i);
-  EXPECT_STREQ("a  b", buf);
+  int i = 1234;
+  EXPECT_EQ(5, snprintf(buf, sizeof(buf), "a %n b", &i));
+  EXPECT_EQ(1234, i);
+  EXPECT_STREQ("a n b", buf);
 }
 
 TEST(stdio, snprintf_smoke) {
