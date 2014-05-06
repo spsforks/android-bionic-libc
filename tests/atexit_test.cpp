5231598c48ccef2add6a6bcc59be2bf303ba35ff
Dmitriy Ivanov <dimitry@google.com>
2014-05-05 19:52:13 -0700

    Add missing test for atexit test suite.
    Change-Id: I3d14d7df1da53ccef534c106633593551ab6a7cb

--- tests/atexit_test.cpp
+++ tests/atexit_test.cpp
@@ -21,17 +21,24 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #include <string>
 
-TEST(atexit, combined_test) {
+#ifdef __LP64__
+#define LP_SUFFIX "64"
+#else
+#define LP_SUFFIX "32"
+#endif
+
+TEST(atexit, dlclose) {
   std::string atexit_call_sequence;
   bool valid_this_in_static_dtor = false;
   void* handle = dlopen("libtest_atexit.so", RTLD_NOW);
-  ASSERT_TRUE(handle != NULL);
+  ASSERT_TRUE(handle != nullptr);
 
   void* sym = dlsym(handle, "register_atexit");
-  ASSERT_TRUE(sym != NULL);
+  ASSERT_TRUE(sym != nullptr);
   reinterpret_cast<void (*)(std::string*, bool*)>(sym)(&atexit_call_sequence, &valid_this_in_static_dtor);
 
   ASSERT_EQ(0, dlclose(handle));
@@ -40,5 +47,51 @@ TEST(atexit, combined_test) {
   ASSERT_TRUE(valid_this_in_static_dtor);
 }
 
-// TODO: test for static dtor calls from from exit(.) -> __cxa_finalize(NULL)
+TEST(atexit, exit) {
+  const std::string test_atexit_expected_output = "";
+  const char* binary_file = "nativetest/test_atexit/test_atexit" LP_SUFFIX;
+
+  char binary_path[PATH_MAX];
+  const char* android_data = getenv("ANDROID_DATA");
+  ASSERT_TRUE(android_data != NULL);
+
+  snprintf(binary_path, sizeof(binary_path), "%s/%s", android_data, binary_file);
+  char* argv[] = { binary_path, nullptr };
+
+  int pipefd[2];
+
+  ASSERT_TRUE(pipe(pipefd) == 0);
+
+  pid_t cpid = fork();
+
+  ASSERT_TRUE(cpid != -1);
+
+  if (cpid == 0) { // child
+    dup2(pipefd[1], 1); // replace stdout
+    // close pipe
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+    ASSERT_TRUE(execvp(binary_path, argv) != -1);
+  } else { // parent
+    close(pipefd[1]);
+    int in = pipefd[0];
+    // read stdout
+    char buf[1024];
+    ssize_t size = 0, len;
+    while ((len=read(in, buf+size, sizeof(buf) - size)) != 0) {
+      if (len == -1 && errno == EAGAIN) {
+          continue;
+      }
+      ASSERT_TRUE(len != -1);
+      size += len;
+    }
+
+    ASSERT_TRUE(static_cast<size_t>(size) < sizeof(buf));
+    int status;
+    wait(&status);
+    ASSERT_EQ(0, status);
+    ASSERT_STREQ("Twas brillig, and the slithy toves did gyre and gimble in the wabe;", buf);
+  }
+}
 
