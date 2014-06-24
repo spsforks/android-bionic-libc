57414b24c93c2ee54c2252589d31462d882f4c3b
Dmitriy Ivanov <dimitry@google.com>
2014-06-24 16:48:46 -0700

    Open libraries using custom lookup function
      android_dlopen_ext() can be instructed to use custom
      library lookup function instead of default one.
    
      do_android_update_lookup_fn can be used to set/reset
      custom lookup function for dlopen(3). When lookup_fn
      is not null dlopen will invoke lookup_fn before anything
      else; if lookup function unable to find the file
      (returns non-zero) dlopen falls back to the default
      lookup order.
    
      This can be used to load library directly from
      apk (see tests for example), or to delegate file-opening
      to another (more privileged) process when running
      isolated_app (b/15984217).
    
    Bug: 8076853
    Bug: 15984217
    Change-Id: I1aa2c039bb2a590ae72f256acc9ba5401c2c59b1

--- tests/Android.mk
+++ tests/Android.mk
@@ -220,6 +220,9 @@ bionic-unit-tests_whole_static_libraries := \
     libBionicTests \
 
 bionic-unit-tests_static_libraries := \
+    libziparchive \
+    libutils \
+    libz \
     libtinyxml2 \
     liblog \
 
@@ -237,6 +240,9 @@ bionic-unit-tests_conlyflags := \
     -fnon-call-exceptions \
 
 bionic-unit-tests_cppflags := $(test_cppflags)
+
+bionic-unit-tests_cppflags_$(TARGET_ARCH) := -DCPU_ABI=$(TARGET_CPU_ABI)
+bionic-unit-tests_cppflags_$(TARGET_2ND_ARCH) := -DCPU_ABI=$(TARGET_2ND_CPU_ABI)
 
 bionic-unit-tests_ldflags := \
     -Wl,--export-dynamic
