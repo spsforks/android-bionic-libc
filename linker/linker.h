ad33cf2ae08d5651bb892e28107d785ee8e374a2
Dmitriy Ivanov <dimitry@google.com>
2014-06-24 16:48:46 -0700

    Open libraries using custom lookup function
      android_dlopen_ext() can be instructed to use custom
      library lookup function instead of default one.
    
      do_android_update_lookup_fn can be used to set/reset
      custom lookup function for dlopen(3). When lookup_fn
      is not null dlopen will invoke lookup_fn before anything
      else; if lookup function unable to find the file
      (returns non-zero) dlopen falls back to the default
      lookup order.
    
      This can be used to load library directly from
      apk (see tests for example), or to delegate file-opening
      to another (more privileged) process when running
      isolated_app (b/15984217).
    
    Bug: 8076853
    Bug: 15984217
    Change-Id: I1aa2c039bb2a590ae72f256acc9ba5401c2c59b1

--- linker/linker.h
+++ linker/linker.h
@@ -335,6 +335,7 @@ soinfo* get_libdl_info();
 
 void do_android_get_LD_LIBRARY_PATH(char*, size_t);
 void do_android_update_LD_LIBRARY_PATH(const char* ld_library_path);
+void do_android_update_lookup_fn(lookup_fn_t lookup_fn);
 soinfo* do_dlopen(const char* name, int flags, const android_dlextinfo* extinfo);
 void do_dlclose(soinfo* si);
 
