31b02998381147d8784e1f6640cda39a9d07c985
Shu Zhang <chazhang@nvidia.com>
2014-07-25 16:24:48 +0800

    libm: arm64: add arm64 specific sqrt/floor/ceil
    Add arm64 specific sqrt/sqrtf, floor/floorf, ceil/ceilf optimized
    implementation.
    
    Change-Id: I3de217a16ad0df5763aad332e528f17918b0ad24

--- libm/arm/arm.mk
+++ libm/arm/arm.mk
@@ -1,8 +1,12 @@
+# ARMv7 processor with VFPv3 and NEON support
 ifneq (generic, $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT))
 libm_arch_src_files_arm := \
     arm/s_floor.S \
     arm/e_sqrt.S \
-    arm/e_sqrtf.S
+    arm/e_sqrtf.S\
+    upstream-freebsd/lib/msun/src/s_ceil.c \
+    upstream-freebsd/lib/msun/src/s_ceilf.c \
+    upstream-freebsd/lib/msun/src/s_floorf.c
 else
 libm_arch_src_files_arm := $(libm_arch_src_files_default)
 endif
