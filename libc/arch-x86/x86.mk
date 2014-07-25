79fd1ee7245390c728437199a48f32816131d29e
Mingwei Shi <mingwei.shi@intel.com>
2014-07-25 08:20:29 +0800

    Add the missed symbols for X86 Silvermont arch
    Due to the 32bit legacy issue, there are some symbols lost in libc.so.
    On X86 Silvermont arch, because of popcnt instruction is supported,
    the building filtered out the symbols __popcountsi2,__popcount_tab, but,
    we also need __popcount* symbols in libc.so and resolve the X86 Silvermont
    compatibility issue.
    
    Change-Id: Ia69b2f561348ccfb6c8775a8e80339082c10991a
    Signed-off-by: Mingwei Shi <mingwei.shi@intel.com>
    Signed-off-by: Varvara Rainchik <varvara.rainchik@intel.com>
    Signed-off-by: Fengwei Yin <fengwei.yin@intel.com>

--- libc/arch-x86/x86.mk
+++ libc/arch-x86/x86.mk
@@ -32,6 +32,11 @@ libc_bionic_src_files_x86 += \
     arch-x86/bionic/sigsetjmp.S \
     arch-x86/bionic/syscall.S \
 
+ifeq ($(ARCH_X86_HAVE_POPCNT)), true)
+libc_bionic_src_files_x86 += \
+    arch-x86/bionic/libgcc_compat.c
+endif
+
 ## ARCH variant specific source files
 arch_variant_mk := $(LOCAL_PATH)/arch-x86/$(TARGET_ARCH_VARIANT)/$(TARGET_ARCH_VARIANT).mk
 ifeq ($(wildcard $(arch_variant_mk)),)
