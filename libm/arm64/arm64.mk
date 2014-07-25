c285e5ff1f3ad2c66d0921e0c7e5470f15034624
Shu Zhang <chazhang@nvidia.com>
2014-07-25 16:24:48 +0800

    libm: arm64: add arm64 specific sqrt/floor/ceil
    Add arm64 specific sqrt/sqrtf, floor/floorf, ceil/ceilf optimized
    implementation.
    
    Change-Id: I3de217a16ad0df5763aad332e528f17918b0ad24

---
+++ libm/arm64/arm64.mk
@@ -0,0 +1,11 @@
+ifneq (generic, $(TARGET_$(my_2nd_arch_prefix)_CPU_VARIANT))
+libm_common_src_files_arm64 := \
+    arm64/s_floor.S \
+    arm64/s_floorf.S \
+    arm64/s_ceil.S \
+    arm64/s_ceilf.S \
+    arm64/e_sqrt.S \
+    arm64/e_sqrtf.S
+else
+libm_common_src_files_arm64 := libm_default_src_files
+endif
