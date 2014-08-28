595ce1728af8de206e969647e9cef0ca4f538dcd
Hans Boehm <hboehm@google.com>
2014-08-28 15:21:32 -0700

    Make stdatomic.h work with gcc4.6 host compiler
    This is needed to make L work correctly, and bionic tests pass
    again, after applying the equivalent of c/104944 there.
    
    It makes the preexisting code that uses __sync implementations
    much more useful, although we should no longer be exercising that
    code in AOSP.
    
    Specifically fixes:
    
    We were invoking __has_extension and __has_builtin for GCC compilations.
    They're clang specific. Restructured the tests.
    
    The __sync implementation was not defining the LOCK_FREE macros.
    
    ATOMIC_VAR_INIT was using named field initializations.  These are a
    C, not C++, feature, that is not supported by g++ 4.6.
    
    The stdatomic bionic test still failed with 4.6 and glibc with our
    questionable LOCK_FREE macro implementation.  Don't run that piece
    with glibc.
    
    In L, this is a prerequisite for fixing b/16880454 and
    
        Bug:16513433
    
    Change-Id: I9b61e42307f96a114dce7552b6ead4ad1c544eab

--- tests/stdatomic_test.cpp
+++ tests/stdatomic_test.cpp
@@ -63,13 +63,15 @@ TEST(stdatomic, atomic_signal_fence) {
 
 TEST(stdatomic, atomic_is_lock_free) {
   atomic_char small;
-  atomic_intmax_t big;
   ASSERT_TRUE(atomic_is_lock_free(&small));
+#if !defined(__GLIBC__) /* TODO: Wait for gcc4.6 prebuilt toolchain to die. */
+  atomic_intmax_t big;
   // atomic_intmax_t(size = 64) is not lock free on mips32.
 #if defined(__mips__) && !defined(__LP64__)
   ASSERT_FALSE(atomic_is_lock_free(&big));
 #else
   ASSERT_TRUE(atomic_is_lock_free(&big));
+#endif
 #endif
 }
 
