772350f29653d78cdc087d8be618ba2078d0d0ac
Derek Xue <derek.xue@linaro.org>
2014-09-25 11:12:01 +0100

    bionic tests: migrate gethostname test to unistd_test.cpp from system/extras
    The old tests are implemented in file
        system/extras/tests/bionic/libc/common/test_gethostname.c
    Here migrate the test to the tests/unistd_test.cpp file and
    add some more checks
    
    Change-Id: Iab1e3da873bb333d1ddefc03108d536933792db2
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- tests/unistd_test.cpp
+++ tests/unistd_test.cpp
@@ -25,6 +25,7 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
+#include <sys/utsname.h>
 #include <sys/wait.h>
 
 TEST(unistd, sysconf_SC_MONOTONIC_CLOCK) {
@@ -466,4 +467,25 @@ TEST(unistd, getpid_caching_and_pthread_create) {
 TEST(unistd_DeathTest, abort) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
   ASSERT_EXIT(abort(), testing::KilledBySignal(SIGABRT), "");
+}
+
+TEST(unistd, gethostname) {
+  char hostname[HOST_NAME_MAX + 1];
+
+  memset(hostname, 0, sizeof(hostname));
+
+  ASSERT_EQ(0, gethostname(hostname, HOST_NAME_MAX));
+  utsname buf;
+  ASSERT_EQ(0, uname(&buf));
+  ASSERT_EQ(0, strncmp(hostname, buf.nodename, SYS_NMLN));
+  ASSERT_GT(strlen(hostname), 0U);
+
+  errno = 0;
+  ASSERT_EQ(-1, gethostname(hostname, strlen(hostname)));
+  ASSERT_EQ(ENAMETOOLONG, errno);
+
+  errno = 0;
+  ASSERT_EQ(0, gethostname(hostname, -2));
+  ASSERT_EQ(0, errno);
+  GTEST_LOG_(INFO) << "hostname=" << hostname << "\n";
 }
