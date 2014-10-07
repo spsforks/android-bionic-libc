3ed6d9406b1ee9c14d4d1ad41c345b56009e43c0
Chih-Hung Hsieh <chh@google.com>
2014-10-07 09:51:33 -0700

    Add missing -fPIC for Clang mips.
    BUG: 17895505
    Change-Id: I146cfede2ed769cbad13ab6114cb1052f65b72cb

--- linker/Android.mk
+++ linker/Android.mk
@@ -45,6 +45,9 @@ LOCAL_NO_CRT := true
 # TODO: split out the asflags.
 LOCAL_ASFLAGS := $(LOCAL_CFLAGS)
 
+# Clang complains about missing -fPIC for mips/begin.S.
+LOCAL_CLANG_ASFLAGS_mips += -fPIC
+
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
 LOCAL_STATIC_LIBRARIES := libc_nomalloc
