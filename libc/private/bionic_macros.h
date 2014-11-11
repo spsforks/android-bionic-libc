645d138e487a4f63dc14f48cfe7243bc49df5ad4
Andreas Gampe <agampe@google.com>
2014-11-10 22:04:08 -0800

    Bionic: Make DISALLOW_COPY_AND_ASSIGN conditional
    Make the definition of DISALLOW_COPY_AND_ASSIGN conditional. This is
    so that the projects that include libnativehelper and bionic macros
    do not have to be careful in which order those projects are included.
    
    Change-Id: Ib12a2c2b7ad2e360edcf3b26cb1be626540fadc1

--- libc/private/bionic_macros.h
+++ libc/private/bionic_macros.h
@@ -17,11 +17,13 @@
 #ifndef _BIONIC_MACROS_H_
 #define _BIONIC_MACROS_H_
 
+#if !defined(DISALLOW_COPY_AND_ASSIGN)
 // DISALLOW_COPY_AND_ASSIGN disallows the copy and operator= functions.
 // It goes in the private: declarations in a class.
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
   TypeName(const TypeName&) = delete;      \
   void operator=(const TypeName&) = delete
+#endif  // !defined(DISALLOW_COPY_AND_ASSIGN)
 
 // A macro to disallow all the implicit constructors, namely the
 // default constructor, copy constructor and operator= functions.
