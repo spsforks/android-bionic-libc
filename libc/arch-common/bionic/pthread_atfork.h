85ea537388ecf2b86da334c7ca0c62f21f13f9ed
Dmitriy Ivanov <dimitry@google.com>
2014-11-20 20:47:02 -0800

    Unregister pthread_atfork handlers on dlclose()
    Bug: http://b/20339788
    Change-Id: I326fdf6bb06bed12743f08980b5c69d849c015b8

--- libc/arch-common/bionic/pthread_atfork.h
+++ libc/arch-common/bionic/pthread_atfork.h
@@ -26,37 +26,15 @@
  * SUCH DAMAGE.
  */
 
-extern void __cxa_finalize(void *);
-extern void *__dso_handle;
+extern void* __dso_handle;
 
-__attribute__((visibility("hidden"),destructor))
-void __on_dlclose() {
-  __cxa_finalize(&__dso_handle);
+extern int __register_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void), void* dso);
+
+#ifndef _LIBC
+// hide it for everyone except libc
+__attribute__ ((visibility ("hidden")))
+#endif
+int pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void)) {
+  return __register_atfork(prepare, parent, child, &__dso_handle);
 }
 
-/* CRT_LEGACY_WORKAROUND should only be defined when building
- * this file as part of the platform's C library.
- *
- * The C library already defines a function named 'atexit()'
- * for backwards compatibility with older NDK-generated binaries.
- *
- * For newer ones, 'atexit' is actually embedded in the C
- * runtime objects that are linked into the final ELF
- * binary (shared library or executable), and will call
- * __cxa_atexit() in order to un-register any atexit()
- * handler when a library is unloaded.
- *
- * This function must be global *and* hidden. Only the
- * code inside the same ELF binary should be able to access it.
- */
-
-#ifdef CRT_LEGACY_WORKAROUND
-# include "__dso_handle.h"
-#else
-# include "__dso_handle_so.h"
-# include "atexit.h"
-#endif
-#include "pthread_atfork.h"
-#ifdef __i386__
-# include "../../arch-x86/bionic/__stack_chk_fail_local.h"
-#endif
