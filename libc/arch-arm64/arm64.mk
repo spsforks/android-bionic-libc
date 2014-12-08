a105e4f41708588e12eaf1112d75c31bc89bc6e1
Richard Earnshaw <rearnsha@arm.com>
2014-12-08 15:21:00 +0000

    libc: ARM64: add assembly strrchr
    Change-Id: I59a0aa618bf8139dc0368af9ddf881eba5d3eadf

--- libc/arch-arm64/arm64.mk
+++ libc/arch-arm64/arm64.mk
@@ -17,6 +17,7 @@ libc_bionic_src_files_arm64 += \
     arch-arm64/generic/bionic/strlen.S \
     arch-arm64/generic/bionic/strncmp.S \
     arch-arm64/generic/bionic/strnlen.S \
+    arch-arm64/generic/bionic/strrchr.S \
     arch-arm64/generic/bionic/wmemmove.S \
 
 libc_bionic_src_files_exclude_arm64 += \
