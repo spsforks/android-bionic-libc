2e6f588de5f582f57c2ebbf6ef3281fc20655a9e
Yabin Cui <yabinc@google.com>
2015-01-05 11:06:30 -0800

    Make sys_resource test more robust.
    "ulimit -c xxx" command may run before bionic-unit-tests.
    Make sure sys_resource test doesn't fail in that case.
    
    Change-Id: Ic3b5ed8b20acba56df8c5ef082c88e5050e761aa

--- tests/sys_resource_test.cpp
+++ tests/sys_resource_test.cpp
@@ -15,7 +15,7 @@
  */
 
 #include <gtest/gtest.h>
-
+#include <errno.h>
 #include <sys/resource.h>
 
 TEST(sys_resource, smoke) {
@@ -35,8 +35,12 @@ TEST(sys_resource, smoke) {
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
-  ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
   ASSERT_EQ(l64.rlim_cur, pr_l64.rlim_cur);
+  if (l64.rlim_cur == RLIM64_INFINITY) {
+    ASSERT_EQ(RLIM_INFINITY, l32.rlim_cur);
+  } else {
+    ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
+  }
   ASSERT_EQ(l64.rlim_max, pr_l64.rlim_max);
   if (l64.rlim_max == RLIM64_INFINITY) {
     ASSERT_EQ(RLIM_INFINITY, l32.rlim_max);
@@ -46,7 +50,8 @@ TEST(sys_resource, smoke) {
 
   // Write with setrlimit and read back with everything.
   l32.rlim_cur = 123;
-  ASSERT_EQ(0, setrlimit(RLIMIT_CORE, &l32));
+  l32.rlim_max = RLIM_INFINITY;
+  ASSERT_EQ(0, setrlimit(RLIMIT_CORE, &l32)) << errno;
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
@@ -56,7 +61,8 @@ TEST(sys_resource, smoke) {
 
   // Write with setrlimit64 and read back with everything.
   l64.rlim_cur = 456;
-  ASSERT_EQ(0, setrlimit64(RLIMIT_CORE, &l64));
+  l64.rlim_max = RLIM64_INFINITY;
+  ASSERT_EQ(0, setrlimit64(RLIMIT_CORE, &l64)) << errno;
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
@@ -65,8 +71,9 @@ TEST(sys_resource, smoke) {
   ASSERT_EQ(l64.rlim_cur, pr_l64.rlim_cur);
 
   // Write with prlimit64 and read back with everything.
-  l64.rlim_cur = 789;
-  ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, &l64, NULL));
+  pr_l64.rlim_cur = 789;
+  pr_l64.rlim_max = RLIM64_INFINITY;
+  ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, &pr_l64, NULL)) << errno;
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
