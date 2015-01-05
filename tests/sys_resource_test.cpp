344d54eb417317446d42bdd9ea272f51824985eb
Yabin Cui <yabinc@google.com>
2015-01-05 11:06:30 -0800

    Make sys_resource test more robust.
    "ulimit -c xxx" command may run before bionic-unit-tests.
    Make sure sys_resource test doesn't fail in that case.
    
    Change-Id: Ic3b5ed8b20acba56df8c5ef082c88e5050e761aa

--- tests/sys_resource_test.cpp
+++ tests/sys_resource_test.cpp
@@ -15,7 +15,8 @@
  */
 
 #include <gtest/gtest.h>
-
+#include <errno.h>
+#include <string.h>
 #include <sys/resource.h>
 
 TEST(sys_resource, smoke) {
@@ -27,6 +28,8 @@ TEST(sys_resource, smoke) {
   ASSERT_EQ(4U, sizeof(rlim_t));
 #endif
 
+  // Hard limit is not changed in the test, as it can only go down for non-root users.
+  // Soft limit should be set between 0 and hard limit.
   // Read with getrlimit, getrlimit64, and prlimit64.
   // (prlimit is prlimit64 on LP64 and unimplemented on 32-bit.)
   rlimit l32;
@@ -35,8 +38,12 @@ TEST(sys_resource, smoke) {
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
-  ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
   ASSERT_EQ(l64.rlim_cur, pr_l64.rlim_cur);
+  if (l64.rlim_cur == RLIM64_INFINITY) {
+    ASSERT_EQ(RLIM_INFINITY, l32.rlim_cur);
+  } else {
+    ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
+  }
   ASSERT_EQ(l64.rlim_max, pr_l64.rlim_max);
   if (l64.rlim_max == RLIM64_INFINITY) {
     ASSERT_EQ(RLIM_INFINITY, l32.rlim_max);
@@ -45,32 +52,40 @@ TEST(sys_resource, smoke) {
   }
 
   // Write with setrlimit and read back with everything.
-  l32.rlim_cur = 123;
-  ASSERT_EQ(0, setrlimit(RLIMIT_CORE, &l32));
+  l32.rlim_cur = l32.rlim_max;
+  ASSERT_EQ(0, setrlimit(RLIMIT_CORE, &l32)) << strerror(errno);
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
-  ASSERT_EQ(123U, l32.rlim_cur);
-  ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
+  ASSERT_EQ(l32.rlim_max, l32.rlim_cur);
+  if (l32.rlim_cur == RLIM_INFINITY) {
+    ASSERT_EQ(RLIM64_INFINITY, l64.rlim_cur);
+  } else {
+    ASSERT_EQ(l32.rlim_cur, l64.rlim_cur);
+  }
   ASSERT_EQ(l64.rlim_cur, pr_l64.rlim_cur);
 
   // Write with setrlimit64 and read back with everything.
-  l64.rlim_cur = 456;
-  ASSERT_EQ(0, setrlimit64(RLIMIT_CORE, &l64));
+  l64.rlim_cur = 0;
+  ASSERT_EQ(0, setrlimit64(RLIMIT_CORE, &l64)) << strerror(errno);
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
-  ASSERT_EQ(456U, l32.rlim_cur);
+  ASSERT_EQ(0U, l32.rlim_cur);
   ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
   ASSERT_EQ(l64.rlim_cur, pr_l64.rlim_cur);
 
   // Write with prlimit64 and read back with everything.
-  l64.rlim_cur = 789;
-  ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, &l64, NULL));
+  pr_l64.rlim_cur = pr_l64.rlim_max;
+  ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, &pr_l64, NULL)) << strerror(errno);
   ASSERT_EQ(0, getrlimit(RLIMIT_CORE, &l32));
   ASSERT_EQ(0, getrlimit64(RLIMIT_CORE, &l64));
   ASSERT_EQ(0, prlimit64(0, RLIMIT_CORE, NULL, &pr_l64));
-  ASSERT_EQ(789U, l32.rlim_cur);
-  ASSERT_EQ(l64.rlim_cur, l32.rlim_cur);
-  ASSERT_EQ(l64.rlim_cur, pr_l64.rlim_cur);
+  ASSERT_EQ(pr_l64.rlim_max, pr_l64.rlim_cur);
+  ASSERT_EQ(pr_l64.rlim_cur, l64.rlim_cur);
+  if (pr_l64.rlim_cur == RLIM64_INFINITY) {
+    ASSERT_EQ(RLIM_INFINITY, l32.rlim_cur);
+  } else {
+    ASSERT_EQ(pr_l64.rlim_cur, l32.rlim_cur);
+  }
 }
