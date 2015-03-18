a559b19a862c1b08641416bad254e3064151aa3d
Yabin Cui <yabinc@google.com>
2015-03-18 14:14:02 -0700

    Make __get_thread inlined.
    Bug: 19825434
    
    Change-Id: Ifb672a45a5776b83625a25654ed0d6f7fc368ae3

--- libc/bionic/ndk_cruft.cpp
+++ libc/bionic/ndk_cruft.cpp
@@ -346,6 +346,13 @@ extern "C" void* dlmalloc(size_t size) {
   return malloc(size);
 }
 
+#include "pthread_internal.h"
+#undef __get_thread
+// Various third-party apps contain a backport of our pthread_rwlock implementation that uses this.
+extern "C" pthread_internal_t* __get_thread(void) {
+  return __get_internal_thread();
+}
+
 #endif // !defined(__LP64__)
 
 // This is never implemented in bionic, only needed for ABI compatibility with the NDK.
