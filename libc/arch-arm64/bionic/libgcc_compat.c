72272fa1f0f187415f295f4dfc8dfc412f9b2350
Dmitriy Ivanov <dimitry@google.com>
2015-04-03 18:36:06 -0700

    Protect unwind symbols
     Stops libc.so from looking for them in
     other shared libraries/main executables.
    
    Bug: http://b/20065774
    Change-Id: Ic71912c60fa8052981417c3fc80c19ddfb54dc22

--- libc/arch-arm64/bionic/libgcc_compat.c
+++ libc/arch-arm64/bionic/libgcc_compat.c
@@ -2,9 +2,9 @@
 
 #if !defined(__clang__)
 
-extern void __clear_cache(char*, char*);
-extern char _Unwind_Backtrace;
-extern char _Unwind_GetIP;
+extern void __clear_cache(char*, char*) __attribute__((visibility("protected")));
+extern char _Unwind_Backtrace __attribute__((visibility("protected")));
+extern char _Unwind_GetIP __attribute__((visibility("protected")));
 
 void* __bionic_libgcc_compat_symbols[] = {
     &__clear_cache,
