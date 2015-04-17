f31f49a60529625b2ab2351dbb4800fda1b4016d
Serban Constantinescu <serban.constantinescu@arm.com>
2015-04-17 19:11:23 +0100

    ARM: Add support for more CPU_VARIANTS.
    This patch adds support for more ARMv7 CPU_VARIANTS as well
    as 32bit (AArch32) and 64bit (AArch64) ARMv8 platforms. For
    now all the new platforms default either to the "generic"
    implementation for arch-arm64 or to the "cortex-a15" one for
    arch-arm.
    
    Change-Id: I474bf14b0313ca890a3342ea119119badcd2028b
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

--- libc/arch-arm/arm.mk
+++ libc/arch-arm/arm.mk
@@ -51,10 +51,21 @@ libc_arch_dynamic_src_files_arm := arch-arm/bionic/exidx_dynamic.c
 ifeq ($(strip $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)),)
   $(warning TARGET_$(my_2nd_arch_prefix)ARCH is arm, but TARGET_$(my_2nd_arch_prefix)CPU_VARIANT is not defined)
 endif
+
 cpu_variant_mk := $(LOCAL_PATH)/arch-arm/$(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)/$(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT).mk
+
 ifeq ($(wildcard $(cpu_variant_mk)),)
-$(error "TARGET_$(my_2nd_arch_prefix)CPU_VARIANT not set or set to an unknown value. Possible values are cortex-a7, cortex-a8, cortex-a9, cortex-a15, krait, denver. Use generic for devices that do not have a CPU similar to any of the supported cpu variants.")
+  # We default to cortex-a15.
+  cpu_variant_mk := $(LOCAL_PATH)/arch-arm/cortex-a15/cortex-a15.mk
+  $(info "Using cortex-a15 optimisations for $(TARGET_$(my_2nd_arch_prefix)CPU_VARIANT)")
 endif
+
+ifeq ($(strip $(TARGET_ARCH)),arm64)
+  ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),generic)
+    cpu_variant_mk := $(LOCAL_PATH)/arch-arm/cortex-a15/cortex-a15.mk
+  endif
+endif
+
 include $(cpu_variant_mk)
 libc_common_additional_dependencies += $(cpu_variant_mk)
 
--- libc/arch-arm64/arm64.mk
+++ libc/arch-arm64/arm64.mk
@@ -52,14 +52,13 @@ libc_crt_target_crtbegin_file_arm64 := \
 libc_crt_target_crtbegin_so_file_arm64 := \
     $(LOCAL_PATH)/arch-common/bionic/crtbegin_so.c
 
-## CPU variant specific source files
-ifeq ($(strip $(TARGET_CPU_VARIANT)),)
-  $(warning TARGET_ARCH is arm64, but TARGET_CPU_VARIANT is not defined)
-endif
 cpu_variant_mk := $(LOCAL_PATH)/arch-arm64/$(TARGET_CPU_VARIANT)/$(TARGET_CPU_VARIANT).mk
+
 ifeq ($(wildcard $(cpu_variant_mk)),)
-$(error "TARGET_CPU_VARIANT not set or set to an unknown value. Possible values are generic, denver64. Use generic for devices that do not have a CPU similar to any of the supported cpu variants.")
+  cpu_variant_mk := $(LOCAL_PATH)/arch-arm64/generic/generic.mk
+  $(info "Using generic optimisations for $(TARGET_CPU_VARIANT)")
 endif
+
 include $(cpu_variant_mk)
 libc_common_additional_dependencies += $(cpu_variant_mk)
 
