2db7fb877e2d1d1b39076bb88ddd2b6bca7e6439
Daniel Micay <danielmicay@gmail.com>
2015-07-18 13:55:51 -0400

    add fortified implementations of fread/fwrite
    A __size_mul_overflow utility is used to take advantage of the checked
    overflow intrinsics in Clang and GCC (>= 5). The fallback for older
    compilers is the optimized but less than ideal overflow checking pattern
    used in OpenBSD.
    
    Change-Id: Ibb0d4fd9b5acb67983e6a9f46844c2fd444f7e69

--- libc/include/sys/cdefs.h
+++ libc/include/sys/cdefs.h
@@ -578,4 +578,20 @@
 #define _BIONIC_NOT_BEFORE_21(x)
 #endif /* __ANDROID_API__ >= 21 */
 
+#if __has_builtin(__builtin_umul_overflow) || __GNUC__ >= 5
+#if __LP64__
+#define __size_mul_overflow(a, b, result) __builtin_umull_overflow(a, b, result)
+#else
+#define __size_mul_overflow(a, b, result) __builtin_umul_overflow(a, b, result)
+#endif
+#else
+#include <stddef.h>
+
+static __inline__ __always_inline int __size_mul_overflow(size_t a, size_t b, size_t *result) {
+    *result = a * b;
+    static const size_t mul_no_overflow = 1UL << (sizeof(size_t) * 4);
+    return (a >= mul_no_overflow || b >= mul_no_overflow) && a > 0 && (size_t)-1 / a < b;
+}
+#endif
+
 #endif /* !_SYS_CDEFS_H_ */
