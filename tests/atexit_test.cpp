dccbb3e3538eeef485ea209b4626078441d7979f
Chih-Hung Hsieh <chh@google.com>
2015-11-16 11:13:04 -0800

    Enable clang to compile more tests.
    * Disable optimization only in gtest.h of atexit_test.c for arm/aarch64
      to keep VTT for std::__1::basic_stringstream<char, std::__1::char_traits<char>,
      std::__1::allocator<char> > to link with g++ compiled modules.
    * bionic-unit-tests source files are not affected by clang x86_64 fp128 bug
      so they can be compiled with clang.
    
    BUG: 25643775
    Change-Id: I3da2a0de61edcdca07b7fcd73a16de9da4a1f7d6

--- tests/atexit_test.cpp
+++ tests/atexit_test.cpp
@@ -14,7 +14,17 @@
  * limitations under the License.
  */
 
+// To work around b/25643775, we disable clang optimization so that
+//   VTT for std::__1::basic_stringstream<char, std::__1::char_traits<char>,
+//   std::__1::allocator<char> >
+// will be correctly kept for other module's references.
+#if __clang__ && (__arm__ || __aarch64__)
+#pragma clang optimize off
+#endif
 #include <gtest/gtest.h>
+#if __clang__ && (__arm__ || __aarch64__)
+#pragma clang optimize on
+#endif
 
 #include <dlfcn.h>
 #include <libgen.h>
