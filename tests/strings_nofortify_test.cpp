91cf4d5c7f6da7b75f33d5222b99b6b537c73fec
Christopher Ferris <cferris@google.com>
2016-01-13 13:47:58 -0800

    Add method to run fortified versions of funcs.
    Add a way to turn fortify off for the files that test fortify functions.
    
    This method involves simply compiling the same file with fortify off and
    changing the test name slightly.
    
    It's not very pretty, and it assumes that only these few files test
    functions that can be fortified.
    
    Bug: 15195631
    Change-Id: Iba9db1d508b7d28a1d6968019cb70fe08864827b

---
+++ tests/strings_nofortify_test.cpp
@@ -0,0 +1,8 @@
+
+#ifdef _FORTIFY_SOURCE
+#undef _FORTIFY_SOURCE
+#endif
+
+#define NOFORTIFY
+
+#include "strings_test.cpp"
---
+++ tests/unistd_nofortify_test.cpp
@@ -0,0 +1,8 @@
+
+#ifdef _FORTIFY_SOURCE
+#undef _FORTIFY_SOURCE
+#endif
+
+#define NOFORTIFY
+
+#include "unistd_test.cpp"
