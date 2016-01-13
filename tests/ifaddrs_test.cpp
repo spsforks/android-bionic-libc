4f6168902ea0a02756fb164b28560186474258ad
Yi Kong <yikong@google.com>
2016-01-13 11:28:14 +0000

    Add more tests for getifaddrs(3)
    This adds the following two checks:
    
    * getifaddrs sees the same list of interfaces as /sys/class/net.
    * IPv4 addresses we get from netdevice(7) agrees with results from
      getifaddrs.
    
    Change-Id: I2f6d79d0b5cde6d98a0f671d1623b6b2bc75b60f

--- tests/ifaddrs_test.cpp
+++ tests/ifaddrs_test.cpp
@@ -18,24 +18,29 @@
 
 #include <ifaddrs.h>
 
+#include <dirent.h>
 #include <linux/if_packet.h>
 #include <net/ethernet.h>
 #include <net/if.h>
 #include <netdb.h>
 #include <netinet/in.h>
 
+#include <algorithm>
+#include <map>
+#include <vector>
+
 TEST(ifaddrs, freeifaddrs_null) {
   freeifaddrs(nullptr);
 }
 
-TEST(ifaddrs, getifaddrs_smoke) {
+// We can't statically say much about what network interfaces are available, but we can be pretty
+// sure there's a loopback interface, and that it has IPv4, IPv6, and AF_PACKET entries.
+TEST(ifaddrs, getifaddrs_lo) {
   ifaddrs* addrs = nullptr;
 
   ASSERT_EQ(0, getifaddrs(&addrs));
   ASSERT_TRUE(addrs != nullptr);
 
-  // We can't say much about what network interfaces are available, but we can be pretty
-  // sure there's a loopback interface, and that it has IPv4, IPv6, and AF_PACKET entries.
   ifaddrs* lo_inet4 = nullptr;
   ifaddrs* lo_inet6 = nullptr;
   ifaddrs* lo_packet = nullptr;
@@ -63,6 +68,108 @@ TEST(ifaddrs, getifaddrs_smoke) {
   ASSERT_EQ(6, sa_ll->sll_halen);
 
   freeifaddrs(addrs);
+}
+
+// Check that getifaddrs sees the same list of interfaces as /sys/class/net.
+TEST(ifaddrs, getifaddrs_interfaces) {
+  std::vector<std::string> ifaddrs_socks;
+  {
+    ifaddrs* addrs;
+    ASSERT_EQ(0, getifaddrs(&addrs));
+
+    for (ifaddrs* addr = addrs; addr != nullptr; addr = addr->ifa_next) {
+      int family = addr->ifa_addr ? addr->ifa_addr->sa_family :
+                                    addr->ifa_broadaddr ? addr->ifa_broadaddr->sa_family :
+                                    AF_UNSPEC;
+
+      if (family == AF_PACKET || family == AF_UNSPEC) {
+        ifaddrs_socks.push_back(std::string(addr->ifa_name));
+      }
+    }
+
+    freeifaddrs(addrs);
+  }
+
+  std::vector<std::string> SYS_CLASS_NET;
+  {
+    DIR *d = opendir("/sys/class/net");
+
+    if (d) {
+      dirent *dir;
+      while ((dir = readdir(d)) != nullptr) {
+        if (dir->d_type == DT_LNK) {
+          SYS_CLASS_NET.push_back(std::string(dir->d_name));
+        }
+      }
+
+      closedir(d);
+    }
+  }
+
+  ASSERT_TRUE(std::is_permutation(ifaddrs_socks.begin(), ifaddrs_socks.end(),
+                                  SYS_CLASS_NET.begin()));
+}
+
+TEST(ifaddrs, getifaddrs_INET) {
+  std::multimap<std::string, in_addr_t> inetaddrs;
+  std::multimap<std::string, in_addr_t> broadinetaddrs;
+
+  ifaddrs* addrs;
+  ASSERT_EQ(0, getifaddrs(&addrs));
+
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  for (ifaddrs* addr = addrs; addr != nullptr; addr = addr->ifa_next) {
+    if (addr->ifa_name && addr->ifa_addr != nullptr && addr->ifa_addr->sa_family == AF_INET) {
+      auto sock = reinterpret_cast<sockaddr_in*>(addr->ifa_addr);
+      inetaddrs.emplace(std::string(addr->ifa_name), sock->sin_addr.s_addr);
+    }
+    if (addr->ifa_name && addr->ifa_broadaddr != nullptr && addr->ifa_broadaddr->sa_family == AF_INET) {
+      auto sock = reinterpret_cast<sockaddr_in*>(addr->ifa_broadaddr);
+      broadinetaddrs.emplace(std::string(addr->ifa_name), sock->sin_addr.s_addr);
+    }
+  }
+
+  // Check that IPv4 addresses we get from ioctl agrees with getifaddrs.
+  for (auto it = inetaddrs.begin(); it != inetaddrs.end(); ) {
+    ifreq ifr;
+    ifr.ifr_addr.sa_family = AF_INET;
+    it->first.copy(ifr.ifr_name, IFNAMSIZ - 1);
+    ioctl(fd, SIOCGIFADDR, &ifr);
+
+    sockaddr_in* sock = reinterpret_cast<sockaddr_in*>(&ifr.ifr_addr);
+    in_addr_t addr = sock->sin_addr.s_addr;
+
+    bool found = false;
+    for (auto ub = inetaddrs.upper_bound(it->first); it != ub; it++) {
+        if (it->second == addr) {
+          found = true;
+        }
+    }
+    ASSERT_TRUE(found);
+  }
+
+  // Do the same for broadcast addresses.
+  for (auto it = broadinetaddrs.begin(); it != broadinetaddrs.end(); ) {
+    ifreq ifr;
+    ifr.ifr_addr.sa_family = AF_INET;
+    it->first.copy(ifr.ifr_name, IFNAMSIZ - 1);
+    ioctl(fd, SIOCGIFBRDADDR, &ifr);
+
+    sockaddr_in* sock = reinterpret_cast<sockaddr_in*>(&ifr.ifr_addr);
+    in_addr_t addr = sock->sin_addr.s_addr;
+
+    bool found = false;
+    for (auto ub = broadinetaddrs.upper_bound(it->first); it != ub; it++) {
+        if (it->second == addr) {
+          found = true;
+        }
+    }
+    ASSERT_TRUE(found);
+  }
+
+  freeifaddrs(addrs);
+  close(fd);
 }
 
 static void print_sockaddr_ll(const char* what, const sockaddr* p) {
