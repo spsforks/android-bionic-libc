b6f348c0147f8cdcf374a7a276baf380d5fdc7dd
Pierre Imai <imaipi@google.com>
2016-04-05 15:49:08 +0900

    Fix off-by-one error in res_cache.c
    Change-Id: I58264902c123b3bc0d392d17837aa537ca0a3ca2

--- libc/dns/resolv/res_send.c
+++ libc/dns/resolv/res_send.c
@@ -491,7 +491,7 @@ res_nsend(res_state statp,
 	    struct __res_stats stats[MAXNS];
 	    struct __res_params params;
 	    int revision_id = _resolv_cache_get_resolver_stats(statp->netid, &params, stats);
-	    bool usable_servers[MAXNS];
+	    bool usable_servers[MAXNS + 1];
 	    _res_stats_get_usable_servers(&params, stats, statp->nscount, usable_servers);
 
 	    for (ns = 0; ns < statp->nscount; ns++) {
