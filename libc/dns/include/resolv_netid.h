04ab9bccafe454076e4cdd3cf88d73c37d0a1398
Pierre Imai <imaipi@google.com>
2016-04-18 11:42:14 +0900

    Store DNS server count in resolv_cache.
    Instead of keeping a sentinel after nameservers[], nsaddrinfo[] and
    nstats[], store the server count in the structure, freeing up memory and
    eliminating the need to enumerate the server count every time
    _resolv_is_nameservers_equal_locked() is invoked.
    
    Also increase MAXNS from 3 to 4.
    
    BUG: 28153323
    Change-Id: I11a7257af695157c9e32019cd00c67b535b63c75

--- libc/dns/include/resolv_netid.h
+++ libc/dns/include/resolv_netid.h
@@ -87,7 +87,7 @@ int android_getaddrinfofornetcontext(const char *, const char *, const struct ad
     const struct android_net_context *, struct addrinfo **) __used_in_netd;
 
 /* set name servers for a network */
-extern void _resolv_set_nameservers_for_net(unsigned netid, const char** servers, int numservers,
+extern int _resolv_set_nameservers_for_net(unsigned netid, const char** servers, int numservers,
 	const char *domains, const struct __res_params* params) __used_in_netd;
 
 /* flush the cache associated with a certain network */
