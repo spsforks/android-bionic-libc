8386a0fc8c773cdf2a30b82a370141e61b5dfcf2
Dimitry Ivanov <dimitry@google.com>
2016-04-21 14:57:38 -0700

    Improve error message for empty list of public libs
    Change-Id: I5a3cbc99d34e05559426b521a964d1407b67a6c4

--- linker/linker.cpp
+++ linker/linker.cpp
@@ -2365,14 +2365,19 @@ int do_dlclose(void* handle) {
 }
 
 bool init_namespaces(const char* public_ns_sonames, const char* anon_ns_library_path) {
-  CHECK(public_ns_sonames != nullptr);
   if (g_public_namespace_initialized) {
     DL_ERR("public namespace has already been initialized.");
     return false;
   }
 
+  if (public_ns_sonames == nullptr || public_ns_sonames[0] == '\0') {
+    DL_ERR("error initializing public namespace: the list of public libraries is empty.");
+    return false;
+  }
+
   std::vector<std::string> sonames = android::base::Split(public_ns_sonames, ":");
 
+  // Split on the empty string results in vector of an empty string.
   ProtectedDataGuard guard;
 
   auto failure_guard = make_scope_guard([&]() {
