8548ddbf5f91083ef16805360292017e5ca55552
Pierre Imai <imaipi@google.com>
2016-04-27 17:30:30 +0900

    Record the resolution start time on errors.
    Also only record samples on the first try for a server.
    
    BUG: 25731675
    
    Change-Id: I10a68813c6636874e63aca1db661e63e1af72a2d

--- libc/dns/resolv/res_send.c
+++ libc/dns/resolv/res_send.c
@@ -552,10 +552,12 @@ res_nsend(res_state statp,
 			n = send_vc(statp, buf, buflen, ans, anssiz, &terrno,
 				    ns, &now, &rcode, &delay);
 
-			struct __res_sample sample;
-			_res_stats_set_sample(&sample, now, rcode, delay);
-			_resolv_cache_add_resolver_stats_sample(statp->netid, revision_id, ns,
-				&sample, params.max_samples);
+			if (try == 0) {
+			    struct __res_sample sample;
+			    _res_stats_set_sample(&sample, now, rcode, delay);
+			    _resolv_cache_add_resolver_stats_sample(statp->netid, revision_id, ns,
+				    &sample, params.max_samples);
+			}
 
 			if (DBG) {
 				__libc_format_log(ANDROID_LOG_DEBUG, "libc",
@@ -576,10 +578,12 @@ res_nsend(res_state statp,
 			n = send_dg(statp, buf, buflen, ans, anssiz, &terrno,
 				    ns, &v_circuit, &gotsomewhere, &now, &rcode, &delay);
 
-			struct __res_sample sample;
-			_res_stats_set_sample(&sample, now, rcode, delay);
-			_resolv_cache_add_resolver_stats_sample(statp->netid, revision_id, ns,
-				&sample, params.max_samples);
+			if (try == 0) {
+			    struct __res_sample sample;
+			    _res_stats_set_sample(&sample, now, rcode, delay);
+			    _resolv_cache_add_resolver_stats_sample(statp->netid, revision_id, ns,
+				    &sample, params.max_samples);
+			}
 
 			if (DBG) {
 				__libc_format_log(ANDROID_LOG_DEBUG, "libc", "used send_dg %d\n",n);
@@ -741,7 +745,7 @@ send_vc(res_state statp,
 	const u_char *buf, int buflen, u_char *ans, int anssiz,
 	int *terrno, int ns, time_t* at, int* rcode, int* delay)
 {
-	*at = 0;
+	*at = time(NULL);
 	*rcode = RCODE_INTERNAL_ERROR;
 	*delay = 0;
 	const HEADER *hp = (const HEADER *)(const void *)buf;
@@ -919,9 +923,6 @@ send_vc(res_state statp,
 		return (0);
 	}
 
-	struct timespec done = evNowTime();
-	*at = done.tv_sec;
-
 	if (truncating) {
 		/*
 		 * Flush rest of answer so connection stays in synch.
@@ -959,6 +960,7 @@ send_vc(res_state statp,
 	 * next nameserver ought not be tried.
 	 */
 	if (resplen > 0) {
+	    struct timespec done = evNowTime();
 	    *delay = _res_stats_calculate_rtt(&done, &now);
 	    *rcode = anhp->rcode;
 	}
@@ -1074,7 +1076,7 @@ send_dg(res_state statp,
 	int *terrno, int ns, int *v_circuit, int *gotsomewhere,
 	time_t *at, int *rcode, int* delay)
 {
-	*at = 0;
+	*at = time(NULL);
 	*rcode = RCODE_INTERNAL_ERROR;
 	*delay = 0;
 	const HEADER *hp = (const HEADER *)(const void *)buf;
@@ -1261,7 +1263,6 @@ retry:
 		goto retry;;
 	}
 	done = evNowTime();
-	*at = done.tv_sec;
 	*delay = _res_stats_calculate_rtt(&done, &now);
 	if (anhp->rcode == SERVFAIL ||
 	    anhp->rcode == NOTIMP ||
