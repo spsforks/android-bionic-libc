1d36a6aa90c63af80158d2d57111d2563c3869a6
Josh Gao <jmgao@google.com>
2016-06-28 15:21:47 -0700

    Move arc4random out of upstream-openbsd, add arc4random_try_buf.
    Take ownership of arc4random, and add a version of arc4random_buf
    that reports failure of entropy gathering rather than just aborting.
    This is useful primarily during early boot on devices that lack
    getrandom(2).
    
    Bug: http://b/29622562
    Change-Id: If44bb571d20313cb7b80223703391de20f56b420

---
+++ libc/private/arc4random_try.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <stddef.h>
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+/*
+ * On systems without getrandom(2), arc4random might not have a real source of
+ * entropy in certain scenarios (early boot before /dev/urandom exists, fd
+ * exhaustion).
+ *
+ * This provides a way for libc entropy users to try arc4random, and fall back
+ * to something else if it's unavailable.
+ *
+ * Returns n if successful, -1 if not.
+ */
+int arc4random_try_buf(void* buf, size_t n);
+
+__END_DECLS
