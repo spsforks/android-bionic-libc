13051a6d4015e883f99d4247b67534639fa9468a
Evgenii Stepanov <eugenis@google.com>
2016-07-06 13:20:59 -0700

    DO NOT COMMIT runtime support for CFI
    Control Flow Integrity support in bionic.
    Diagnostic mode not supported yet.
    
    General design:
    http://clang.llvm.org/docs/ControlFlowIntegrityDesign.html#shared-library-support
    
    This CL implements subsections "CFI Shadow" and "CFI_SlowPath" in the above document.
    
    Bug: 22033465
    Change-Id: I14dfea630de468eb5620e7f55f92b1397ba06217

--- linker/dlfcn.cpp
+++ linker/dlfcn.cpp
@@ -197,11 +197,14 @@ static const char ANDROID_LIBDL_STRTAB[] =
   // 00000000001 1111111112222222222 3333333333444444444455555555556666666666777 777777788888888889999999999
   // 01234567890 1234567890123456789 0123456789012345678901234567890123456789012 345678901234567890123456789
     "erate_phdr\0android_dlopen_ext\0android_set_application_target_sdk_version\0android_get_application_tar"
-  // 0000000000111111 111122222222223333333333 4444444444555555555566666 6666677
-  // 0123456789012345 678901234567890123456789 0123456789012345678901234 5678901
-    "get_sdk_version\0android_init_namespaces\0android_create_namespace\0dlvsym\0"
+  // 0000000000111111 111122222222223333333333 4444444444555555555566666 6666677 777777778888888 8889999999999
+  // 0123456789012345 678901234567890123456789 0123456789012345678901234 5678901 234567890123456 7890123456789
+    "get_sdk_version\0android_init_namespaces\0android_create_namespace\0dlvsym\0__cfi_slowpath\0__cfi_slowpat"
+  // 0000000
+  // 0123456
+    "h_diag\0"
 #if defined(__arm__)
-  // 272
+  // 307
     "dl_unwind_find_exidx\0"
 #endif
     ;
@@ -226,8 +229,10 @@ static ElfW(Sym) g_libdl_symtab[] = {
   ELFW(SYM_INITIALIZER)(216, &android_init_namespaces, 1),
   ELFW(SYM_INITIALIZER)(240, &android_create_namespace, 1),
   ELFW(SYM_INITIALIZER)(265, &dlvsym, 1),
+  ELFW(SYM_INITIALIZER)(272, &__cfi_slowpath, 1),
+  ELFW(SYM_INITIALIZER)(287, &__cfi_slowpath_diag, 1),
 #if defined(__arm__)
-  ELFW(SYM_INITIALIZER)(272, &dl_unwind_find_exidx, 1),
+  ELFW(SYM_INITIALIZER)(307, &dl_unwind_find_exidx, 1),
 #endif
 };
 
@@ -244,9 +249,9 @@ static ElfW(Sym) g_libdl_symtab[] = {
 // Note that adding any new symbols here requires stubbing them out in libdl.
 static unsigned g_libdl_buckets[1] = { 1 };
 #if defined(__arm__)
-static unsigned g_libdl_chains[] = { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0 };
+static unsigned g_libdl_chains[] = { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 0 };
 #else
-static unsigned g_libdl_chains[] = { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 0 };
+static unsigned g_libdl_chains[] = { 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 0 };
 #endif
 
 static uint8_t __libdl_info_buf[sizeof(soinfo)] __attribute__((aligned(8)));
