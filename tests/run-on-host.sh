72ee7d8519b93e85f54b213df24a3af77475e7c8
Colin Cross <ccross@android.com>
2016-07-11 16:20:06 -0700

    Convert bionic benchmarks and tests to Android.bp
    The compile-time tests and a few custom libraries for dynamic linker
    testing are still compiled in make.
    
    Also converts the make rules to run tests on the host to shell scripts
    in tests/run-on-host.sh and benchmarks/run-on-host.sh
    
    Change-Id: I6f174b3a69d58c4ed74d29f4e79332d483681534

---
+++ tests/run-on-host.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+. $(dirname $0)/../build/run-on-host.sh
+
+if [ "$1" = glibc ]; then
+    m -j bionic-unit-tests-glibc
+    (
+        cd ${ANDROID_BUILD_TOP}
+        export ANDROID_DATA=${TARGET_OUT_DATA}
+        export ANDROID_ROOT=${TARGET_OUT}
+        ${HOST_OUT}/nativetest64/bionic-unit-tests-glibc/bionic-unit-tests-glibc $@
+    )
+    exit 0
+fi
+
+if [ ${HOST_OS}-${HOST_ARCH} = linux-x86 -o ${HOST_OS}-${HOST_ARCH} = linux-x86_64 ]; then
+
+    prepare bionic-unit-tests
+
+    if [ ${TARGET_ARCH} = x86 -o ${TARGET_ARCH} = x86_64 ]; then
+        (
+            cd ${ANDROID_BUILD_TOP}
+            export ANDROID_DATA=${TARGET_OUT_DATA}
+            export ANDROID_DNS_MODE=local
+            export ANDROID_ROOT=${TARGET_OUT}
+            ${NATIVETEST}/bionic-unit-tests/bionic-unit-tests $@
+        )
+    fi
+fi
