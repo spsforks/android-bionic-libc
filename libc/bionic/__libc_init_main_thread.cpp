aosp-mirror/platform_bionic

commit 78a52f19bb207d1c736f1d5362e01f338d78645f

Attempt to use arc4random where getauxval(AT_RANDOM) is used.
When arc4random can get entropy (which is true for basically everyone
but init on kernels that don't support getrandom), use it instead of
AT_RANDOM.

Bug: http://b/29622562
Change-Id: I6932803af2c477e65562ff531bd959f199fad1df

libc/bionic/__libc_init_main_thread.cpp
@@ -29,6 +29,7 @@
 #include "libc_init_common.h"
 
 #include "private/KernelArgumentBlock.h"
+#include "private/bionic_arc4random.h"
 #include "private/bionic_auxv.h"
 #include "private/bionic_globals.h"
 #include "private/bionic_ssp.h"
@@ -41,9 +42,7 @@ extern "C" int __set_tid_address(int* tid_address);
 uintptr_t __stack_chk_guard = 0;
 
 void __libc_init_global_stack_chk_guard(KernelArgumentBlock& args) {
-  // AT_RANDOM is a pointer to 16 bytes of randomness on the stack.
-  // Take the first 4/8 for the -fstack-protector implementation.
-  __stack_chk_guard = *reinterpret_cast<uintptr_t*>(args.getauxval(AT_RANDOM));
+  __libc_safe_arc4random_buf(&__stack_chk_guard, sizeof(__stack_chk_guard), args);
 }
 
 // Setup for the main thread. For dynamic executables, this is called by the

libc/Android.bp
@@ -1211,6 +1211,7 @@ cc_library_static {
         "bionic/arpa_inet.cpp",
         "bionic/assert.cpp",
         "bionic/atof.cpp",
+        "bionic/bionic_arc4random.cpp",
         "bionic/bionic_netlink.cpp",
         "bionic/bionic_systrace.cpp",
         "bionic/bionic_time_conversions.cpp",

libc/bionic/bionic_arc4random.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2016 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,44 +26,33 @@
  * SUCH DAMAGE.
  */
 
-#include <assert.h>
-#include <errno.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/auxv.h>
-#include <sys/cdefs.h>
-
 #include "private/bionic_arc4random.h"
-#include "private/bionic_globals.h"
-#include "private/libc_logging.h"
+
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/auxv.h>
+#include <syscall.h>
+#include <unistd.h>
+
 #include "private/KernelArgumentBlock.h"
+#include "private/libc_logging.h"
 
-void __libc_init_setjmp_cookie(libc_globals* globals, KernelArgumentBlock& args) {
-  long value;
-  __libc_safe_arc4random_buf(&value, sizeof(value), args);
+void __libc_safe_arc4random_buf(void* buf, size_t n, KernelArgumentBlock& args) {
+  static bool have_getrandom = syscall(SYS_getrandom, nullptr, 0, 0) == -1 && errno != ENOSYS;
+  static bool have_urandom = access("/dev/urandom", R_OK) == 0;
+  static size_t at_random_bytes_consumed = 0;
 
-  // Mask off the last bit to store the signal flag.
-  globals->setjmp_cookie = value & ~1;
-}
-
-extern "C" __LIBC_HIDDEN__ long __bionic_setjmp_cookie_get(long sigflag) {
-  if (sigflag & ~1) {
-    __libc_fatal("unexpected sigflag value: %ld", sigflag);
+  if (have_getrandom || have_urandom) {
+    arc4random_buf(buf, n);
+    return;
   }
 
-  return __libc_globals->setjmp_cookie | sigflag;
-}
-
-// Aborts if cookie doesn't match, returns the signal flag otherwise.
-extern "C" __LIBC_HIDDEN__ long __bionic_setjmp_cookie_check(long cookie) {
-  if (__libc_globals->setjmp_cookie != (cookie & ~1)) {
-    __libc_fatal("setjmp cookie mismatch");
+  if (at_random_bytes_consumed + n > 16) {
+    __libc_fatal("ran out of AT_RANDOM bytes, have %zu, requested %zu",
+                 16 - at_random_bytes_consumed, n);
   }
 
-  return cookie & 1;
-}
-
-extern "C" __LIBC_HIDDEN__ long __bionic_setjmp_checksum_mismatch() {
-  __libc_fatal("setjmp checksum mismatch");
+  memcpy(buf, reinterpret_cast<char*>(args.getauxval(AT_RANDOM)) + at_random_bytes_consumed, n);
+  at_random_bytes_consumed += n;
+  return;
 }

libc/private/bionic_arc4random.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2016 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,44 +26,20 @@
  * SUCH DAMAGE.
  */
 
-#include <assert.h>
-#include <errno.h>
-#include <stddef.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/auxv.h>
-#include <sys/cdefs.h>
+#ifndef _PRIVATE_BIONIC_ARC4RANDOM_H_
+#define _PRIVATE_BIONIC_ARC4RANDOM_H_
 
-#include "private/bionic_arc4random.h"
-#include "private/bionic_globals.h"
-#include "private/libc_logging.h"
+#include <stddef.h>
+
 #include "private/KernelArgumentBlock.h"
 
-void __libc_init_setjmp_cookie(libc_globals* globals, KernelArgumentBlock& args) {
-  long value;
-  __libc_safe_arc4random_buf(&value, sizeof(value), args);
+/*
+ * arc4random aborts if it's unable to fetch entropy, which is always the case
+ * for init on devices without getrandom(2), since /dev/random hasn't been
+ * created yet. Provide a wrapper function that falls back to AT_RANDOM if
+ * we don't have getrandom and /dev/urandom is missing.
+ */
 
-  // Mask off the last bit to store the signal flag.
-  globals->setjmp_cookie = value & ~1;
-}
+void __libc_safe_arc4random_buf(void* buf, size_t n, KernelArgumentBlock& args);
 
-extern "C" __LIBC_HIDDEN__ long __bionic_setjmp_cookie_get(long sigflag) {
-  if (sigflag & ~1) {
-    __libc_fatal("unexpected sigflag value: %ld", sigflag);
-  }
-
-  return __libc_globals->setjmp_cookie | sigflag;
-}
-
-// Aborts if cookie doesn't match, returns the signal flag otherwise.
-extern "C" __LIBC_HIDDEN__ long __bionic_setjmp_cookie_check(long cookie) {
-  if (__libc_globals->setjmp_cookie != (cookie & ~1)) {
-    __libc_fatal("setjmp cookie mismatch");
-  }
-
-  return cookie & 1;
-}
-
-extern "C" __LIBC_HIDDEN__ long __bionic_setjmp_checksum_mismatch() {
-  __libc_fatal("setjmp checksum mismatch");
-}
+#endif

spsforks/android-bionic-libc

commit 3a699b7803d36a61abda4cf766cff105fe3d3487

Initialize main thread TLS before the global stack guard.
The GCE breakage caused by 78a52f19bb207d1c736f1d5362e01f338d78645 was
due to TLS_SLOT_SELF being uninitialized before the use of errno by
syscall. Separate stack guard initialization from TLS initialization so
that stack gurad initialization can make syscalls.

Bug: http://b/29622562
Bug: http://b/31251721
Change-Id: Id0e4379e0efb7194a2df7bd16211ff11c6598033

libc/bionic/__libc_init_main_thread.cpp
@@ -87,13 +87,14 @@ void __libc_init_main_thread(KernelArgumentBlock& args) {
   main_thread.attr.stack_size = 0; // User code should never see this; we'll compute it when asked.
   // TODO: the main thread's sched_policy and sched_priority need to be queried.
 
+  __init_thread(&main_thread);
+  __init_tls(&main_thread);
+
   // The TLS stack guard is set from the global, so ensure that we've initialized the global
   // before we initialize the TLS. Dynamic executables will initialize their copy of the global
   // stack protector from the one in the main thread's TLS.
   __libc_init_global_stack_chk_guard(args);
-
-  __init_thread(&main_thread);
-  __init_tls(&main_thread);
+  __init_thread_stack_guard(&main_thread);
 
   // Store a pointer to the kernel argument block in a TLS slot to be
   // picked up by the libc constructor.

libc/bionic/pthread_create.cpp
@@ -56,7 +56,6 @@ void __init_tls(pthread_internal_t* thread) {
   // Slot 0 must point to itself. The x86 Linux kernel reads the TLS from %fs:0.
   thread->tls[TLS_SLOT_SELF] = thread->tls;
   thread->tls[TLS_SLOT_THREAD_ID] = thread;
-  __init_thread_stack_guard(thread);
 }
 
 void __init_thread_stack_guard(pthread_internal_t* thread) {
@@ -182,6 +181,7 @@ static int __allocate_thread(pthread_attr_t* attr, pthread_internal_t** threadp,
   thread->mmap_size = mmap_size;
   thread->attr = *attr;
   __init_tls(thread);
+  __init_thread_stack_guard(thread);
 
   *threadp = thread;
   *child_stack = stack_top;