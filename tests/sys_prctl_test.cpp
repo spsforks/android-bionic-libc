aaedb44a5d3e58ced2e8a839353cfbb1f31171d2
Christopher Ferris <cferris@google.com>
2016-11-03 16:06:40 -0700

    Make prctl test check maps.
    It turns out that sometimes this kernel bug can be present but not
    trigger a crash. Modify the test to check for overlapping maps instead.
    
    Bug: 32640542
    
    Test: Verified that this fails on a device without the kernel patch
    Test: (both bionic-unit-tests32 and CtsBionicTestCases32). Verified
    Test: this passes on an angler.
    
    Change-Id: Ia3fae3ee3d01f33d8414c1796fc3e942d5b56ec8

--- tests/sys_prctl_test.cpp
+++ tests/sys_prctl_test.cpp
@@ -14,11 +14,17 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
-
+#include <inttypes.h>
+#include <stdio.h>
 #include <sys/mman.h>
 #include <sys/prctl.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+#include <vector>
+
+#include <gtest/gtest.h>
+
 #include "private/bionic_prctl.h"
 
 // http://b/20017123.
@@ -29,9 +35,26 @@ TEST(sys_prctl, bug_20017123) {
   ASSERT_NE(MAP_FAILED, p);
   ASSERT_EQ(0, mprotect(p, page_size, PROT_NONE));
   ASSERT_NE(-1, prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, p, page_size * 3, "anonymous map space"));
-  volatile char* vp = reinterpret_cast<volatile char*>(p);
-  // Below memory access causes SEGV if the memory map is screwed up.
-  *(vp + page_size) = 0;
+  // Now read the maps and verify that there are no overlapped maps.
+  FILE* maps_file = fopen("/proc/self/maps", "r");
+  ASSERT_TRUE(maps_file != nullptr);
+
+  std::vector<char> line(8192);
+  std::vector<char> previous_line;
+  uintptr_t last_start = 0;
+  uintptr_t last_end = 0;
+  while (fgets(line.data(), line.size(), maps_file)) {
+    uintptr_t start;
+    uintptr_t end;
+    ASSERT_EQ(2, sscanf(line.data(), "%" SCNxPTR "-%" SCNxPTR " ", &start, &end));
+    ASSERT_GE(start, last_end)
+        << "Overlapping map detected:\n" << previous_line.data() << line.data();
+    previous_line = line;
+    last_start = start;
+    last_end = end;
+  }
+  fclose(maps_file);
+
   ASSERT_EQ(0, munmap(p, page_size * 3));
 #else
   GTEST_LOG_(INFO) << "This test does nothing as it tests an Android specific kernel feature.";
