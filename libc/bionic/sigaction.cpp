6728874a240ce9d550a7c90549f6e8cb2c918fad
Evgenii Stepanov <eugenis@google.com>
2016-11-11 15:48:47 -0800

    DO NOT COMMIT WiP sigaltstack for safestack
    Test: bionic device tests
    
    Change-Id: I24e71761c6840f63d9fb6c763b1bc35af2f34546

--- libc/bionic/sigaction.cpp
+++ libc/bionic/sigaction.cpp
@@ -26,20 +26,71 @@
  * SUCH DAMAGE.
  */
 
+#include <assert.h>
 #include <signal.h>
+#include "bionic/pthread_internal.h"
 
 extern "C" void __restore_rt(void);
 extern "C" void __restore(void);
 
 #if defined(__LP64__)
 
+#ifdef BIONIC_SAFESTACK
+typedef void (*sigaction_t) (int, struct siginfo*, void*);
+static atomic_uintptr_t real_sighandlers[NSIG];
+
+__attribute__((no_sanitize("safe-stack")))
+static void sigaction_wrapper(int signum, struct siginfo* si, void* uc) {
+  assert(signum >= 0 && signum < NSIG);
+  void *altstack_start;
+  size_t altstack_size;
+  __get_thread()->unsafe_altstack_get(&altstack_start, &altstack_size);
+  void*& unsafe_stack_ptr = __get_tls()[TLS_SLOT_SAFESTACK];
+  bool switch_stacks = false;
+  if (altstack_start && altstack_size) {
+    uintptr_t left = reinterpret_cast<uintptr_t>(altstack_start);
+    uintptr_t right = left + altstack_size;
+    uintptr_t cur = reinterpret_cast<uintptr_t>(unsafe_stack_ptr);
+    switch_stacks = (cur < left) || (cur >= right);
+  }
+  sigaction_t real_sigaction = reinterpret_cast<sigaction_t>(atomic_load(&real_sighandlers[signum]));
+  if (switch_stacks) {
+    void* saved_unsafe_stack_ptr = unsafe_stack_ptr;
+    unsafe_stack_ptr =
+        reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(altstack_start) + altstack_size);
+    real_sigaction(signum, si, uc);
+    unsafe_stack_ptr = saved_unsafe_stack_ptr;
+  } else {
+    real_sigaction(signum, si, uc);
+  }
+}
+
+static bool wrap_signal(int signal, const struct sigaction* bionic_new_action) {
+  if (!bionic_new_action) return false;
+  sighandler_t new_handler = bionic_new_action->sa_handler;
+  if (new_handler == SIG_DFL || new_handler == SIG_IGN)
+    return false;
+  if (signal >= NSIG)
+    return false;
+  return true;
+}
+#else // BIONIC_SAFESTACK
+static bool wrap_signal(int signal, const struct sigaction* bionic_new_action) {
+  return false;
+}
+#endif // BIONIC_SAFESTACK
+
 extern "C" int __rt_sigaction(int, const struct __kernel_sigaction*, struct __kernel_sigaction*, size_t);
 
 int sigaction(int signal, const struct sigaction* bionic_new_action, struct sigaction* bionic_old_action) {
   __kernel_sigaction kernel_new_action;
-  if (bionic_new_action != NULL) {
+  sighandler_t wrapper = reinterpret_cast<sighandler_t>(sigaction_wrapper);
+  sighandler_t new_handler = nullptr;
+  bool wrap = wrap_signal(signal, bionic_new_action);
+  if (bionic_new_action != nullptr) {
+    new_handler = bionic_new_action->sa_handler;
     kernel_new_action.sa_flags = bionic_new_action->sa_flags;
-    kernel_new_action.sa_handler = bionic_new_action->sa_handler;
+    kernel_new_action.sa_handler = wrap ? wrapper : bionic_new_action->sa_handler;
     kernel_new_action.sa_mask = bionic_new_action->sa_mask;
 #if defined(SA_RESTORER)
     kernel_new_action.sa_restorer = bionic_new_action->sa_restorer;
@@ -59,13 +110,20 @@ int sigaction(int signal, const struct sigaction* bionic_new_action, struct siga
 
   __kernel_sigaction kernel_old_action;
   int result = __rt_sigaction(signal,
-                              (bionic_new_action != NULL) ? &kernel_new_action : NULL,
-                              (bionic_old_action != NULL) ? &kernel_old_action : NULL,
+                              (bionic_new_action != nullptr) ? &kernel_new_action : nullptr,
+                              (bionic_old_action != nullptr) ? &kernel_old_action : nullptr,
                               sizeof(sigset_t));
 
-  if (bionic_old_action != NULL) {
+  if (result == 0 && wrap) {
+    atomic_store(&real_sighandlers[signal], reinterpret_cast<uintptr_t>(new_handler));
+  }
+
+  if (bionic_old_action != nullptr) {
     bionic_old_action->sa_flags = kernel_old_action.sa_flags;
-    bionic_old_action->sa_handler = kernel_old_action.sa_handler;
+    bionic_old_action->sa_handler =
+        (kernel_old_action.sa_handler == wrapper)
+            ? reinterpret_cast<sighandler_t>(atomic_load(&real_sighandlers[signal]))
+            : kernel_old_action.sa_handler;
     bionic_old_action->sa_mask = kernel_old_action.sa_mask;
 #if defined(SA_RESTORER)
     bionic_old_action->sa_restorer = kernel_old_action.sa_restorer;
@@ -83,7 +141,7 @@ int sigaction(int signal, const struct sigaction* bionic_new_action, struct siga
   // The 32-bit ABI is broken. struct sigaction includes a too-small sigset_t,
   // so we have to use sigaction(2) rather than rt_sigaction(2).
   struct sigaction kernel_new_action;
-  if (bionic_new_action != NULL) {
+  if (bionic_new_action != nullptr) {
     kernel_new_action.sa_flags = bionic_new_action->sa_flags;
     kernel_new_action.sa_handler = bionic_new_action->sa_handler;
     kernel_new_action.sa_mask = bionic_new_action->sa_mask;
@@ -96,7 +154,7 @@ int sigaction(int signal, const struct sigaction* bionic_new_action, struct siga
     }
 #endif
   }
-  return __sigaction(signal, (bionic_new_action != NULL) ? &kernel_new_action : NULL, bionic_old_action);
+  return __sigaction(signal, (bionic_new_action != nullptr) ? &kernel_new_action : nullptr, bionic_old_action);
 }
 
 #endif
