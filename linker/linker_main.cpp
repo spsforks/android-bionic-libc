a5b9ff80a0cc4aa5babb1a624b9e37654f145c95
Elliott Hughes <enh@google.com>
2017-01-09 14:39:44 -0800

    Dynamic linker errors should go to stderr.
    Addresses review comment.
    
    Bug: N/A
    Test: manual
    Change-Id: I7c997a71e6e02381d21ac762595ba90370db9d05

--- linker/linker_main.cpp
+++ linker/linker_main.cpp
@@ -316,7 +316,7 @@ static ElfW(Addr) __linker_init_post_relocation(KernelArgumentBlock& args, ElfW(
     // tombstone for them. The tombstone never provided any detail relevant to
     // fixing the problem anyway, and the utility of drawing extra attention
     // to the problem is non-existent at this late date.
-    __libc_format_fd(STDOUT_FILENO,
+    __libc_format_fd(STDERR_FILENO,
                      "\"%s\": error: Android 5.0 and later only support "
                      "position-independent executables (-fPIE).",
                      g_argv[0]);
