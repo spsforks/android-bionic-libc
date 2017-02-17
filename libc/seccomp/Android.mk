c4075e9fc859eb9f267044245e8514be0fbb6d91
Paul Lawrence <paullawrence@google.com>
2017-02-17 10:22:03 -0800

    Support all architectures in seccomp
    Test: Make sure arm, x86, x86_64 emulators boot
          Make sure sailfish still boots
          Cannot work out how to run mips emulators, just hoping for the
          best
    
    Change-Id: Iddee5acdb19ed32c7bd4657573313ca439cf6a49

--- libc/seccomp/Android.mk
+++ libc/seccomp/Android.mk
@@ -5,9 +5,18 @@ include $(CLEAR_VARS)
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 LOCAL_MODULE := libseccomp_policy
 LOCAL_CLANG := true
-LOCAL_SRC_FILES := seccomp_policy.cpp arm_policy.cpp arm64_policy.cpp
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_SHARED_LIBRARIES := libbase
+LOCAL_SRC_FILES := seccomp_policy.cpp \
+		   arm_policy.cpp \
+		   arm64_policy.cpp \
+		   mips_policy.cpp \
+		   mips64_policy.cpp \
+		   x86_policy.cpp \
+		   x86_64_policy.cpp \
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+LOCAL_CFLAGS := -DTARGET_ARCH_$(TARGET_ARCH) -DTARGET_2ND_ARCH_$(TARGET_2ND_ARCH)
 
 include $(BUILD_STATIC_LIBRARY)
 
