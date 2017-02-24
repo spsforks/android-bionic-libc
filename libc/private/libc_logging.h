f470878f4ec36b535ec99497c9084143c25c07ce
Dimitry Ivanov <dimitry@google.com>
2017-02-23 17:57:14 -0800

    libc: __system_property_set uses writev to write atomically
    __system_property_set sometimes produces broken_pipe error
    when trying to write a property.
    
    This change improves error messages and uses writev() instead
    of sequence of send() calls.
    
    Bug: http://b/35381074
    Test: bionic-unit-tests --gtest_filter=prop*
    Change-Id: I7a5b169c015db4e6b720370e58662de8206d1086

--- libc/private/libc_logging.h
+++ libc/private/libc_logging.h
@@ -97,6 +97,13 @@ int __libc_format_log_va_list(int pri, const char* _Nonnull tag, const char* _No
 #endif
 int __libc_write_log(int pri, const char* _Nonnull tag, const char* _Nonnull msg);
 
+#define CHECK(predicate) { \
+    if (!(predicate)) { \
+      __libc_fatal("%s:%d: %s CHECK '" #predicate "' failed", \
+          __FILE__, __LINE__, __FUNCTION__); \
+    } \
+  }
+
 __END_DECLS
 
 #endif
