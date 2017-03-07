2fd3365b849d3bb35331bded61c779f14aee72e1
Josh Gao <jmgao@google.com>
2017-03-06 17:45:33 -0800

    Increase signal stack size on 32-bit to 16kB.
    snprintf to a buffer of length PATH_MAX immediately blows out the stack
    on 32-bit devices. Increase the stack size to compensate.
    
    Bug: http://b/35858739
    Test: none
    Change-Id: I34a7f42c1fd2582ca0d0a9b7e7a5290bc1cc19b1

--- libc/bionic/pthread_internal.h
+++ libc/bionic/pthread_internal.h
@@ -141,13 +141,9 @@ static inline __always_inline bionic_tls& __get_bionic_tls() {
 
 __LIBC_HIDDEN__ void pthread_key_clean_all(void);
 
-#if defined(__LP64__)
-// SIGSTKSZ is not big enough for 64-bit arch.
+// SIGSTKSZ is not big enough.
 // See https://code.google.com/p/android/issues/detail?id=187064.
 #define SIGNAL_STACK_SIZE_WITHOUT_GUARD_PAGE (16 * 1024)
-#else
-#define SIGNAL_STACK_SIZE_WITHOUT_GUARD_PAGE SIGSTKSZ
-#endif
 
 /*
  * Traditionally we gave threads a 1MiB stack. When we started
