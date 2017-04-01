438b1173f0c71ee8c469e9272ebe67c96a39dc75
Nick Kralevich <nnk@google.com>
2017-04-01 09:54:19 -0700

    Add ANDROID_DNS_MODE and BIONIC_DNSCACHE to UNSAFE_VARIABLE_NAMES
    For security reasons, when a binary is executed which causes a security
    transition (eg, a setuid binary, setgid binary, filesystem capabilities,
    or SELinux domain transition), the AT_SECURE flag is set. This causes
    certain blacklisted environment variables to be stripped before the
    process is executed. The list of blacklisted environment variables is
    stored in UNSAFE_VARIABLE_NAMES.
    
    Add ANDROID_DNS_MODE and BIONIC_DNSCACHE to the list of unsafe variables.
    Similar to RESOLV_HOST_CONF and RES_OPTIONS (which are already
    blacklisted), these variables controls how name resolution requests are
    handled. Allowing ANDROID_DNS_MODE or BIONIC_DNSCACHE to be set across a
    security boundary could induce resolution failures or otherwise impact
    name resolution.
    
    Test: Android compiles and runs with no obvious problems.
    Change-Id: I835a7b42d6afbc9c67866594c7951cfd9b355d81

--- libc/bionic/libc_init_common.cpp
+++ libc/bionic/libc_init_common.cpp
@@ -244,6 +244,8 @@ static bool __is_unsafe_environment_variable(const char* name) {
   // of executing a setuid program or the result of an SELinux
   // security transition.
   static constexpr const char* UNSAFE_VARIABLE_NAMES[] = {
+    "ANDROID_DNS_MODE",
+    "BIONIC_DNSCACHE",
     "GCONV_PATH",
     "GETCONF_DIR",
     "HOSTALIASES",
