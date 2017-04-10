e0dda60d4c2f0200cc2f5e731149b7bcfbdc0e37
Jake Weinstein <jake@aospa.co>
2017-04-10 11:22:26 -0400

    libc: arm: Add optimized memchr implementations from newlib
    Enable significantly faster NEON routine for targets that require it
    and generic ARM assembly for Cortex-A9 and generic.
    
    Test: mm
    Change-Id: Ief05eb5cbf3f47d80bf63a8cc256da0d3d5df041

--- libc/arch-arm/dynamic_function_dispatch.cpp
+++ libc/arch-arm/dynamic_function_dispatch.cpp
@@ -154,6 +154,11 @@ static CpuVariant get_cpu_variant() {
     return cpu_variant;
 }
 
+typedef void* memchr_func(void*, const void*, size_t);
+DEFINE_IFUNC(memchr) {
+    RETURN_FUNC(memchr_func, memchr_a15);
+}
+
 typedef void* memmove_func(void* __dst, const void* __src, size_t __n);
 DEFINE_IFUNC(memmove) {
     RETURN_FUNC(memmove_func, memmove_a15);
