80d09cebbdda5affb9a6278c70143e474fda938c
Dan Albert <danalbert@google.com>
2017-08-04 16:15:28 -0700

    Add FIXED_IN() and platform_version().
    Needed for making some tests pass on old devices.
    
    Test: make checkbuild
    Bug: https://github.com/android-ndk/ndk/issues/424
    Change-Id: If0cd8131576cbea08c805029c6248f38607e29d2

---
+++ tests/PlatformVersion.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "PlatformVersion.h"
+
+#if defined(__ANDROID__)
+#include <sys/system_properties.h>
+
+#include <string>
+
+int platform_version() {
+  static int platform_version_ = 0;
+  if (platform_version_ == 0) {
+    char value[PROP_VALUE_MAX];
+    if (__system_property_get("ro.build.version.sdk", value) == 0) {
+      platform_version_ = -1;
+    } else {
+      platform_version_ = std::stoi(value);
+    }
+  }
+
+  return platform_version_;
+}
+#endif  // defined(__ANDROID__)
