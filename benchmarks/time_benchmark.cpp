96903f93eb9e0c3ee0b82c2988f574755bcef0d1
Mark Salyzyn <salyzyn@google.com>
2017-11-07 13:48:46 -0800

    bionic: benchmark: add clock_getres performance tests
    Provide a means to check vdso kernel performance for all reasoned
    combinations of clock_getres, same set of ids as clock_gettime.
    
    Test: /data/nativetest{64}/bionic-benchmarks-tests/bionic-benchmarks-tests
          /data/benchmarktest{64}/bionic-benchmarks/bionic-benchmarks \
            --benchmark_filter=BM_time_clock_getres*
    Bug: 63737556
    Change-Id: I11ea200f67aec2a2f6ad9284960f2941298da222

--- benchmarks/time_benchmark.cpp
+++ benchmarks/time_benchmark.cpp
@@ -22,6 +22,11 @@
 #include <benchmark/benchmark.h>
 #include "util.h"
 
+#ifndef CLOCK_TAI     // This is a reserved value that may not land in headers.
+#define CLOCK_TAI 11  // clock_gettime vdso does not support it anyways, so
+#endif                // helps measure syscall supported overhead.
+#define CLOCK_unsupported 32639  // unsupported by both vdso and syscall
+
 static void BM_time_clock_gettime(benchmark::State& state) {
   timespec t;
   while (state.KeepRunning()) {
@@ -57,7 +62,7 @@ BIONIC_BENCHMARK(BM_time_clock_gettime_BOOTTIME);
 static void BM_time_clock_gettime_TAI(benchmark::State& state) {
   timespec t;
   while (state.KeepRunning()) {
-    clock_gettime(11, &t);
+    clock_gettime(CLOCK_TAI, &t);
   }
 }
 BIONIC_BENCHMARK(BM_time_clock_gettime_TAI);
@@ -65,10 +70,58 @@ BIONIC_BENCHMARK(BM_time_clock_gettime_TAI);
 static void BM_time_clock_gettime_unsupported(benchmark::State& state) {
   timespec t;
   while (state.KeepRunning()) {
-    clock_gettime(32639, &t);
+    clock_gettime(clock_unsupported, &t);
   }
 }
 BIONIC_BENCHMARK(BM_time_clock_gettime_unsupported);
+
+static void BM_time_clock_getres(benchmark::State& state) {
+  timespec t;
+  while (state.KeepRunning()) {
+    clock_getres(CLOCK_MONOTONIC, &t);
+  }
+}
+BIONIC_BENCHMARK(BM_time_clock_getres);
+
+static void BM_time_clock_getres_syscall(benchmark::State& state) {
+  timespec t;
+  while (state.KeepRunning()) {
+    syscall(__NR_clock_getres, CLOCK_MONOTONIC, &t);
+  }
+}
+BIONIC_BENCHMARK(BM_time_clock_getres_syscall);
+
+static void BM_time_clock_getres_REALTIME(benchmark::State& state) {
+  timespec t;
+  while (state.KeepRunning()) {
+    clock_getres(CLOCK_REALTIME, &t);
+  }
+}
+BIONIC_BENCHMARK(BM_time_clock_getres_REALTIME);
+
+static void BM_time_clock_getres_BOOTTIME(benchmark::State& state) {
+  timespec t;
+  while (state.KeepRunning()) {
+    clock_getres(CLOCK_BOOTTIME, &t);
+  }
+}
+BIONIC_BENCHMARK(BM_time_clock_getres_BOOTTIME);
+
+static void BM_time_clock_getres_TAI(benchmark::State& state) {
+  timespec t;
+  while (state.KeepRunning()) {
+    clock_getres(CLOCK_TAI, &t);
+  }
+}
+BIONIC_BENCHMARK(BM_time_clock_getres_TAI);
+
+static void BM_time_clock_getres_unsupported(benchmark::State& state) {
+  timespec t;
+  while (state.KeepRunning()) {
+    clock_getres(CLOCK_unsupported, &t);
+  }
+}
+BIONIC_BENCHMARK(BM_time_clock_getres_unsupported);
 
 static void BM_time_gettimeofday(benchmark::State& state) {
   timeval tv;
