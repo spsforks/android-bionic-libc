652d2b3a30e97ced1c1ea2c3891f897ae5c0cffa
Elliott Hughes <enh@google.com>
2018-01-24 18:54:38 -0800

    Better handling of sigset_t on LP32.
    The main motivation here is that the sigprocmask in pthread_exit wasn't
    actually blocking the real-time signals, and debuggerd (amongst other
    things) is using them. I wasn't able to write a test that actually won
    that race but I did write an equivalent one for posix_spawn.
    
    This also fixes all the uses of sigset_t where the sigset_t isn't
    exposed to the outside (which we can't easily fix because it would be
    an ABI change).
    
    Bug: https://issuetracker.google.com/72291624
    Test: ran tests
    Change-Id: Ib6eebebc5a7b0150079f1cb79593247917dcf750

--- libc/bionic/posix_timers.cpp
+++ libc/bionic/posix_timers.cpp
@@ -75,7 +75,8 @@ static void* __timer_thread_start(void* arg) {
   PosixTimer* timer = reinterpret_cast<PosixTimer*>(arg);
 
   kernel_sigset_t sigset;
-  sigaddset(sigset.get(), TIMER_SIGNAL);
+  sigset.clear();
+  sigset.set(TIMER_SIGNAL);
 
   while (true) {
     // Wait for a signal...
@@ -151,13 +152,14 @@ int timer_create(clockid_t clock_id, sigevent* evp, timer_t* timer_id) {
   // We start the thread with TIMER_SIGNAL blocked by blocking the signal here and letting it
   // inherit. If it tried to block the signal itself, there would be a race.
   kernel_sigset_t sigset;
-  sigaddset(sigset.get(), TIMER_SIGNAL);
+  sigset.clear();
+  sigset.set(TIMER_SIGNAL);
   kernel_sigset_t old_sigset;
-  pthread_sigmask(SIG_BLOCK, sigset.get(), old_sigset.get());
+  __rt_sigprocmask(SIG_BLOCK, &sigset, &old_sigset, sizeof(sigset));
 
   int rc = pthread_create(&timer->callback_thread, &thread_attributes, __timer_thread_start, timer);
 
-  pthread_sigmask(SIG_SETMASK, old_sigset.get(), NULL);
+  __rt_sigprocmask(SIG_SETMASK, &old_sigset, nullptr, sizeof(sigset));
 
   if (rc != 0) {
     free(timer);
