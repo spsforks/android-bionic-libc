2056c6670ef7b387badd053e05be1a99a2b055b2
dimitry <dimitry@google.com>
2018-06-06 11:11:25 +0200

    Remove check on number of segments in a .so file
    This check was not necessary since it is perfectly fine for a dso to
    have more that 3 PT_LOAD segments.
    
    Bug: http://b/109747297
    Test: make
    Change-Id: I0e3b400fddf05ed7b9342bacbfffd3b578f26cf3

--- tests/dlext_test.cpp
+++ tests/dlext_test.cpp
@@ -1921,7 +1921,6 @@ TEST(dlext, ns_anonymous) {
   // some sanity checks..
   ASSERT_TRUE(addr_start > 0);
   ASSERT_TRUE(addr_end > 0);
-  ASSERT_EQ(3U, maps_to_copy.size());
   ASSERT_TRUE(ns_get_dlopened_string_addr > addr_start);
   ASSERT_TRUE(ns_get_dlopened_string_addr < addr_end);
 
