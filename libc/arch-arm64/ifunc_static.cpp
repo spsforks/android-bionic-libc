ed3997c5e73920be3c29d9bfa5bb8b147cae20be
Haibo Huang <hhb@google.com>
2018-09-17 16:26:54 -0700

    ifunc test (not for review)
    (Just a quick presubmit run to see whether it breaks any device)
    
    Test: Hmm...
    Change-Id: I4c76420cf781d6f6992e9c2facf838e0fafe381e

---
+++ libc/arch-arm64/ifunc_static.cpp
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <sys/syscall.h>
+
+extern "C" {
+
+__attribute__((visibility("hidden")))
+int __memcmp_generic(const void* __lhs, const void* __rhs, size_t __n);
+
+int memcmp(const void* __lhs, const void* __rhs, size_t __n) {
+    return __memcmp_generic(__lhs, __rhs, __n);
+}
+
+}
