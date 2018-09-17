bff3d1dc8ae1a7b12d8afe8d686d2794f36193da
Haibo Huang <hhb@google.com>
2018-09-17 16:26:54 -0700

    ifunc test (not for review)
    (Just a quick presubmit run to see whether it breaks any device)
    
    Test: Hmm...
    Change-Id: I4c76420cf781d6f6992e9c2facf838e0fafe381e

---
+++ libc/arch-arm64/ifunc.cpp
@@ -0,0 +1,57 @@
+#include <fcntl.h>
+#include <sys/syscall.h>
+
+extern "C" {
+
+/*
+static char* hardware = nullptr;
+static char buf[4096];
+
+static long __always_inline __open(const char *pathname, int flags) {
+    register long r0 __asm__("r0") = AT_FDCWD;
+    register long r1 __asm__("r1") = reinterpret_cast<long>(pathname);
+    register long r2 __asm__("r2") = flags;
+    register long r3 __asm__("r3") = 0;
+    register long r7 __asm__("r7") = __NR_openat;
+    __asm__("svc #0" : "=r"(r0) : "r"(r0), "r"(r1), "r"(r2), "r"(r3), "r"(r7));
+    return r0;
+}
+
+static ssize_t __always_inline __read(int fd, void *buf, size_t count) {
+    register long r0 __asm__("r0") = fd;
+    register long r1 __asm__("r1") = reinterpret_cast<long>(buf);
+    register long r2 __asm__("r2") = count;
+    register long r7 __asm__("r7") = __NR_read;
+    __asm__("svc #0" : "=r"(r0) : "r"(r0), "r"(r1), "r"(r2), "r"(r7) : "memory");
+    return r0;
+}
+
+static char* get_hardware_name() {
+    if (hardware != nullptr) return hardware;
+
+    int fd = __open("/proc/cpuinfo", O_RDONLY);
+    if (fd == -1) return buf;
+
+    __read(fd, buf, 4096);
+    return buf;
+}
+*/
+
+typedef int (*memcmp_ptr)(const void* __lhs, const void* __rhs, size_t __n);
+
+__attribute__((visibility("hidden")))
+int __memcmp_generic(const void* __lhs, const void* __rhs, size_t __n);
+
+int memcmp(const void* __lhs, const void* __rhs, size_t __n)
+        __attribute__((ifunc ("memcmp_resolver")));
+
+memcmp_ptr memcmp_resolver() {
+    // get_hardware_name();
+    // if (buf[0] != 0) {
+        return __memcmp_generic;
+    // } else {
+    //    return nullptr;
+    //}
+}
+
+}
