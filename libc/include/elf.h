abbfd33c3c06de78c9852d76ffdb7a391a049f71
Stephen Crane <sjc@immunant.com>
2019-01-18 15:12:29 -0800

    Bionic loader implementation of pagerando segment loading
    Pagerando is an ABI extension that allows the loader to map specially marked
    segments at a random offset from all other segments in a DSO. See
    https://github.com/immunant/android_llvm/wiki/Pagerando-RFC for more
    details. Pagerando requires that the compiler cooperate to prepare code for
    randomized loading. A prototype LLVM LTO implementation of a
    pagerando-compatible compiler is available at
    https://github.com/immunant/android_llvm/ until it can be upstreamed.
    
    Test: Full build with pagerando enabled. walleye boots and
    runs. bionic-unit-tests pass
    
    Change-Id: Ief5052d48046a6731d46218001f47c750bace8d3

--- libc/include/elf.h
+++ libc/include/elf.h
@@ -472,6 +472,10 @@ typedef Elf64_Xword Elf64_Relr;
 #define PF_MASKOS   0x0ff00000
 #define PF_MASKPROC 0xf0000000
 
+/* Android specific for Pagerando. This segment can be mapped at an arbitrary
+ * offset from all other segments in the library. */
+#define PF_RAND_ADDR 0x00100000
+
 #define PT_GNU_RELRO 0x6474e552
 
 #define STB_LOOS 10
