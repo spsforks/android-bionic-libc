aadb3c5bd76e5f8f2e36947b4b6ee6d3ccf81ad7
Vic Yang <victoryang@google.com>
2019-01-28 23:04:40 +0000

    Revert "linker: Purge block allocator memory when possible"
    This reverts commit fb78a4ac1b93218f59aa44089ae5f4dbfababf0d.
    
    Reason for revert: Performance regression.
    
    Change-Id: Ib12335fc7478dad933da00b8bc525366c9330a17

--- linker/linker_block_allocator.cpp
+++ linker/linker_block_allocator.cpp
@@ -52,8 +52,7 @@ LinkerBlockAllocator::LinkerBlockAllocator(size_t block_size)
   : block_size_(
       round_up(block_size < sizeof(FreeBlockInfo) ? sizeof(FreeBlockInfo) : block_size, 16)),
     page_list_(nullptr),
-    free_block_list_(nullptr),
-    allocated_(0)
+    free_block_list_(nullptr)
 {}
 
 void* LinkerBlockAllocator::alloc() {
@@ -73,8 +72,6 @@ void* LinkerBlockAllocator::alloc() {
   }
 
   memset(block_info, 0, block_size_);
-
-  ++allocated_;
 
   return block_info;
 }
@@ -104,11 +101,6 @@ void LinkerBlockAllocator::free(void* block) {
   block_info->num_free_blocks = 1;
 
   free_block_list_ = block_info;
-
-  --allocated_;
-  if (allocated_ == 0) {
-    free_all_pages();
-  }
 }
 
 void LinkerBlockAllocator::protect_all(int prot) {
@@ -158,19 +150,4 @@ LinkerBlockAllocatorPage* LinkerBlockAllocator::find_page(void* block) {
   }
 
   abort();
-}
-
-void LinkerBlockAllocator::free_all_pages() {
-  if (allocated_) {
-    abort();
-  }
-
-  LinkerBlockAllocatorPage* page = page_list_;
-  while (page) {
-    LinkerBlockAllocatorPage* next = page->next;
-    munmap(page, PAGE_SIZE);
-    page = next;
-  }
-  page_list_ = nullptr;
-  free_block_list_ = nullptr;
 }
