deff1decfb2a2281cc5468a9ebb7766d5ffdab00
Daniel Mentz <danielmentz@google.com>
2019-03-28 18:12:30 -0700

    clean_header: Don't print extraneous newline char
    Don't print an extraneous newline character at the end of the include
    file if clean_header.py is used in the non-update mode. This is
    necessary to achieve consistent results in the update and non-update
    modes.
    
    Running clean_header in the non-update mode and redirecting the output
    to a file should have the same result as using the update mode and have
    the script write to the file directly.
    
    Change-Id: I6b176c5365840d66e4499bacd205f1fa77302a2b

--- libc/kernel/tools/clean_header.py
+++ libc/kernel/tools/clean_header.py
@@ -172,7 +172,9 @@ if __name__ == "__main__":
             dst_file = os.path.join(dst_dir, path)
             src_file = os.path.join(src_dir, path)
             new_data = cleanupFile(dst_file, src_file, path)
-            print new_data
+            # Use sys.stdout.write instead of a simple print statement to avoid
+            # outputting an extra new line character at the end.
+            sys.stdout.write(new_data)
 
         sys.exit(0)
 
