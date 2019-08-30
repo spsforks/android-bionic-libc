879971cacc5b6efbadc235cdf29574789a816c4e
Elliott Hughes <enh@google.com>
2019-08-29 20:45:14 -0700

    Support statvfs on API levels before 19.
    Split statfs and statvfs. The former has been available forever, and the
    latter is implemented in terms of the former. The implementation has
    been moved into headers so that it can be used at low API levels.
    
    There's no reason for any Android or Linux code to use statvfs rather
    than statfs, but code that needs to build on Darwin too will want to use
    statvfs because Darwin's statfs is very spartan.
    
    Bug: https://github.com/android-ndk/ndk/issues/609
    Test: treehugger
    Change-Id: Icf3d5723a260099fddb2d9f902e3047b0f041647

--- libc/bionic/statvfs.cpp
+++ libc/bionic/statvfs.cpp
@@ -16,76 +16,19 @@
 
 #include <sys/statvfs.h>
 
-#include <sys/statfs.h>
+// libc++ uses statvfs (for Darwin compatibility), but on Linux statvfs is
+// just another name for statfs, so it didn't arrive until API level 19. We
+// make the implementation available as inlines to support std::filesystem
+// for NDK users (see https://github.com/android-ndk/ndk/issues/609).
 
-// Paper over the fact that 32-bit kernels use fstatfs64/statfs64 with an extra argument,
-// but 64-bit kernels don't have the "64" bit suffix or the extra size_t argument.
-#if defined(__LP64__)
-extern "C" int __fstatfs(int, struct statfs*);
-extern "C" int __statfs(const char*, struct statfs*);
-#  define __fstatfs64(fd,size,buf) __fstatfs(fd,buf)
-#  define __statfs64(path,size,buf) __statfs(path,buf)
-#else
-extern "C" int __fstatfs64(int, size_t, struct statfs*);
-extern "C" int __statfs64(const char*, size_t, struct statfs*);
-#endif
+#define __BIONIC_SYS_STATVFS_INLINE /* Out of line. */
+#include <bits/sys_statvfs_inlines.h>
 
-// The kernel sets a private ST_VALID flag to signal to the C library whether the
-// f_flags field is valid. This flag should not be exposed to users of the C library.
-#define ST_VALID 0x0020
+// Historically we provided actual symbols for statvfs64 and fstatvfs64.
+// They're not particularly useful, but we can't take them away.
 
-static void __statfs_to_statvfs(const struct statfs& in, struct statvfs* out) {
-  out->f_bsize = in.f_bsize;
-  out->f_frsize = in.f_frsize;
-  out->f_blocks = in.f_blocks;
-  out->f_bfree = in.f_bfree;
-  out->f_bavail = in.f_bavail;
-  out->f_files = in.f_files;
-  out->f_ffree = in.f_ffree;
-  out->f_favail = in.f_ffree;
-  out->f_fsid = in.f_fsid.__val[0] | (static_cast<uint64_t>(in.f_fsid.__val[1]) << 32);
-  out->f_flag = in.f_flags;
-  out->f_namemax = in.f_namelen;
-}
-
-int fstatfs(int fd, struct statfs* result) {
-  int rc = __fstatfs64(fd, sizeof(*result), result);
-  if (rc != 0) {
-    return rc;
-  }
-  result->f_flags &= ~ST_VALID;
-  return 0;
-}
-__strong_alias(fstatfs64, fstatfs);
-
-int statfs(const char* path, struct statfs* result) {
-  int rc = __statfs64(path, sizeof(*result), result);
-  if (rc != 0) {
-    return rc;
-  }
-  result->f_flags &= ~ST_VALID;
-  return 0;
-}
-__strong_alias(statfs64, statfs);
-
-int statvfs(const char* path, struct statvfs* result) {
-  struct statfs tmp;
-  int rc = statfs(path, &tmp);
-  if (rc != 0) {
-    return rc;
-  }
-  __statfs_to_statvfs(tmp, result);
-  return 0;
-}
+#undef statvfs64
 __strong_alias(statvfs64, statvfs);
 
-int fstatvfs(int fd, struct statvfs* result) {
-  struct statfs tmp;
-  int rc = fstatfs(fd, &tmp);
-  if (rc != 0) {
-    return rc;
-  }
-  __statfs_to_statvfs(tmp, result);
-  return 0;
-}
+#undef fstatvfs64
 __strong_alias(fstatvfs64, fstatvfs);
