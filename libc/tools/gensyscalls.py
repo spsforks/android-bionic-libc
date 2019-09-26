6d514fbff97f4d5e8d567033f275215a3f331120
John Cater <jcater@google.com>
2019-09-26 14:47:03 -0400

    Fix gensyscalls.py to actually use the input argument provided in bionic/libc/Android.bp.
    Test: Built and tested bionic.
    Change-Id: Ibb25990b2b1b5c18edfdaaab4f1593fa8d95f338

--- libc/tools/gensyscalls.py
+++ libc/tools/gensyscalls.py
@@ -489,9 +489,9 @@ class SysCallsTxtParser:
             self.parse_open_file(fp)
 
 
-def main(arch):
+def main(arch, syscall_file):
     parser = SysCallsTxtParser()
-    parser.parse_file(os.path.join(bionic_libc, "SYSCALLS.TXT"))
+    parser.parse_file(syscall_file)
 
     for syscall in parser.syscalls:
         syscall["__NR_name"] = make__NR_name(syscall["name"])
@@ -528,4 +528,13 @@ def main(arch):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if len(sys.argv) == 0:
+      print "Usage: gensyscalls.py ARCH [SOURCE_FILE]"
+      sys.exit(1)
+
+    arch = sys.argv[1]
+    if len(sys.argv) > 1:
+      syscall_file = sys.argv[2]
+    else:
+      syscall_file = os.path.join(bionic_libc, "SYSCALLS.TXT")
+    main(arch, syscall_file)
