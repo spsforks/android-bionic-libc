0eccd4131c4d6be282a969d35828dacc839189df
Peter Collingbourne <pcc@google.com>
2019-12-20 15:41:37 -0800

    Delete unnecessary (and sometimes problematic) attributes on libdl{,_android} functions.
    The weak attributes on declarations were causing ld-android.so to be
    dropped from libdl{,_android}.so's DT_NEEDED at link time when linked with
    --as-needed. Somehow things kept working most of the time, but it caused
    some binaries to start segfaulting at startup.
    
    While here, drop the visibility attributes on the declarations (which are
    redundant, declarations have default visibility by default) as well as the
    weak attributes on the definitions (weak has no effect on defined symbols
    at the dynamic loader level).
    
    We can delete the weak attribute on dl_iterate_phdr because libdl.cpp is no
    longer used to produce libdl.a, only libdl.so.
    
    Bug: 146456667
    Change-Id: I452af9a2b605bca3a8432fc40d112c83cbd858df

--- libdl/libdl.cpp
+++ libdl/libdl.cpp
@@ -24,116 +24,88 @@
 
 extern "C" {
 
-__attribute__((__weak__, visibility("default")))
 void __loader_android_get_LD_LIBRARY_PATH(char* buffer, size_t buffer_size);
 
-__attribute__((__weak__, visibility("default")))
 void* __loader_dlopen(const char* filename, int flags, const void* caller_addr);
 
-__attribute__((__weak__, visibility("default")))
 char* __loader_dlerror();
 
-__attribute__((__weak__, visibility("default")))
 void* __loader_dlsym(void* handle, const char* symbol, const void* caller_addr);
 
-__attribute__((__weak__, visibility("default")))
 void* __loader_dlvsym(void* handle,
                       const char* symbol,
                       const char* version,
                       const void* caller_addr);
 
-__attribute__((__weak__, visibility("default")))
 int __loader_dladdr(const void* addr, Dl_info* info);
 
-__attribute__((__weak__, visibility("default")))
 int __loader_dlclose(void* handle);
 
 #if defined(__arm__)
-__attribute__((__weak__, visibility("default")))
 _Unwind_Ptr __loader_dl_unwind_find_exidx(_Unwind_Ptr pc, int* pcount);
 #endif
 
-__attribute__((__weak__, visibility("default")))
 int __loader_dl_iterate_phdr(int (*cb)(struct dl_phdr_info* info, size_t size, void* data),
                              void* data);
 
-__attribute__((__weak__, visibility("default")))
 void __loader_android_get_LD_LIBRARY_PATH(char* buffer, size_t buffer_size);
 
-__attribute__((__weak__, visibility("default")))
 void __loader_android_update_LD_LIBRARY_PATH(const char* ld_library_path);
 
-__attribute__((__weak__, visibility("default")))
 void* __loader_android_dlopen_ext(const char* filename,
                                   int flag,
                                   const android_dlextinfo* extinfo,
                                   const void* caller_addr);
 
-__attribute__((__weak__, visibility("default")))
 int __loader_android_get_application_target_sdk_version();
 
 // Proxy calls to bionic loader
-__attribute__((__weak__))
 void android_get_LD_LIBRARY_PATH(char* buffer, size_t buffer_size) {
   __loader_android_get_LD_LIBRARY_PATH(buffer, buffer_size);
 }
 
-__attribute__((__weak__))
 void* dlopen(const char* filename, int flag) {
   const void* caller_addr = __builtin_return_address(0);
   return __loader_dlopen(filename, flag, caller_addr);
 }
 
-__attribute__((__weak__))
 char* dlerror() {
   return __loader_dlerror();
 }
 
-__attribute__((__weak__))
 void* dlsym(void* handle, const char* symbol) {
   const void* caller_addr = __builtin_return_address(0);
   return __loader_dlsym(handle, symbol, caller_addr);
 }
 
-__attribute__((__weak__))
 void* dlvsym(void* handle, const char* symbol, const char* version) {
   const void* caller_addr = __builtin_return_address(0);
   return __loader_dlvsym(handle, symbol, version, caller_addr);
 }
 
-__attribute__((__weak__))
 int dladdr(const void* addr, Dl_info* info) {
   return __loader_dladdr(addr, info);
 }
 
-__attribute__((__weak__))
 int dlclose(void* handle) {
   return __loader_dlclose(handle);
 }
 
 #if defined(__arm__)
-__attribute__((__weak__))
 _Unwind_Ptr dl_unwind_find_exidx(_Unwind_Ptr pc, int* pcount) {
   return __loader_dl_unwind_find_exidx(pc, pcount);
 }
 #endif
 
-/*
- * This needs to be defined as weak because it is also defined in libc.a.
- * Without this, static executables will have a multiple definition error.
- */
-__attribute__((__weak__))
 int dl_iterate_phdr(int (*cb)(struct dl_phdr_info* info, size_t size, void* data), void* data) {
   return __loader_dl_iterate_phdr(cb, data);
 }
 
-__attribute__((__weak__))
 void* android_dlopen_ext(const char* filename, int flag, const android_dlextinfo* extinfo) {
   const void* caller_addr = __builtin_return_address(0);
   return __loader_android_dlopen_ext(filename, flag, extinfo, caller_addr);
 }
 
-__attribute__((__weak__))
 int android_get_application_target_sdk_version() {
   return __loader_android_get_application_target_sdk_version();
 }
