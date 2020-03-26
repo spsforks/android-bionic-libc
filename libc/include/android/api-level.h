79950178abd6200c93fd73e005a3694e91c875d3
Dan Albert <danalbert@google.com>
2020-03-26 16:18:13 -0700

    Add API constant for S.
    Test: treehugger
    Bug: http://b/152037166
    Change-Id: I9039377e80574cd59e13726a12072fe3002df67d

--- libc/include/android/api-level.h
+++ libc/include/android/api-level.h
@@ -110,6 +110,9 @@ __BEGIN_DECLS
 /** Names the "R" API level (30), for comparison against `__ANDROID_API__`. */
 #define __ANDROID_API_R__ 30
 
+/** Names the "S" API level (3100), for comparison against `__ANDROID_API__`. */
+#define __ANDROID_API_S__ 3100
+
 /**
  * Returns the `targetSdkVersion` of the caller, or `__ANDROID_API_FUTURE__`
  * if there is no known target SDK version (for code not running in the
