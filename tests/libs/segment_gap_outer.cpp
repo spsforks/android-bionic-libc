6c5f32a677cff96b4cf58092f7a5a8ab31b5f5e3
Stephen Hines <srhines@google.com>
2020-07-23 19:35:49 -0700

    Specify sections to merge in segment_gap_outer.lds more precisely
    https://reviews.llvm.org/D75225 changed the way that orphan sections are
    retained, breaking this test. The test relied on these sections being
    merged in an implementation-defined order that no longer holds true.
    
    Bug: http://b/161943302
    Test: adb shell /data/nativetest64/bionic-unit-tests/bionic-unit-tests --gtest_filter=dlfcn.segment_gap --no_isolate
    Change-Id: I65656080e39be16833191cb92d3d4c41e409b216

--- tests/libs/segment_gap_outer.cpp
+++ tests/libs/segment_gap_outer.cpp
@@ -2,7 +2,7 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 
-extern "C" void text_before_start_of_gap() {}
+extern "C" void __attribute__((section(".text_before_start_of_gap"))) foo() {}
 char end_of_gap[0x1000];
 
 extern "C" void* get_inner() {
@@ -10,7 +10,7 @@ extern "C" void* get_inner() {
   info.flags = ANDROID_DLEXT_RESERVED_ADDRESS;
 
   char* start_of_gap =
-      reinterpret_cast<char*>(reinterpret_cast<uintptr_t>(text_before_start_of_gap) & ~0xfffull) +
+      reinterpret_cast<char*>(reinterpret_cast<uintptr_t>(foo) & ~0xfffull) +
       0x1000;
   info.reserved_addr = start_of_gap;
   info.reserved_size = end_of_gap - start_of_gap;
