7b4e304833ee4acfd9ccf63cded5278132051e48
Almaz Mingaleev <mingaleev@google.com>
2022-02-11 12:27:12 +0000

    Bump tzcode from 2016g to 2022a*.
    Upstream has renamed tzsetlcl to tzset_unlocked. As bionic's
    implementation of tzset_unlock differs from upstream, these changes were
    skipped.
    
    Also, upstream has removed constants (SECSPERMIN, etc) from tzfile.h. As
    they are used in strptime.c, I've decided to leave them in tzfile.h and
    to not bring them into strptime.c.
    
    HAVE_TZNAME and USG_COMPAT flags semantics were updated, thus setting
    their values to 2 in Android.bp file. See
    https://github.com/eggert/tz/commit/1a27ec76bc436a64070461bbbf28e0511c7cf3b8
    
    * Also https://github.com/eggert/tz/commit/4742526b7e8a0f26adaf068c9f5c0bc0870909e7
    was picked up, which is not part of 2022a.
    
    Changes were applied using following commands:
      1) Checkout tzcode repo
      2) Prepare patches for all tzcode file using
        git diff 2016g 2021e -- <file-name> > <file-name-patch>
      3) Apply these patches to files in bionic using
        patch -p1 <file-name> <file-name-patch>
    
    Bug: 25413083
    Test: CtsLibcoreTestCases
    Test: CtsLibcoreOjTestCases
    Test: CtsBionicTestCases
    
    Change-Id: I9aba4cbeab30171a32f94d20c8e4057804a4c01f

--- tests/time_test.cpp
+++ tests/time_test.cpp
@@ -136,7 +136,7 @@ TEST(time, mktime_empty_TZ) {
 TEST(time, mktime_10310929) {
   struct tm t;
   memset(&t, 0, sizeof(tm));
-  t.tm_year = 200;
+  t.tm_year = 2099 - 1900;
   t.tm_mon = 2;
   t.tm_mday = 10;
 
@@ -151,14 +151,62 @@ TEST(time, mktime_10310929) {
   setenv("TZ", "America/Los_Angeles", 1);
   tzset();
   errno = 0;
-  ASSERT_EQ(static_cast<time_t>(4108348800U), mktime(&t));
+  // On a date/time specified by t America/Los_Angeles
+  // follows DST. But tm_isdst is set to 0, which forces
+  // mktime to assume that time is standard, hence offset
+  // is 8 hours, not 7.
+  ASSERT_EQ(static_cast<time_t>(4076812800U), mktime(&t));
   ASSERT_EQ(0, errno);
+
+  // mktime call above sets t.tm_isdst to 1 as time specified
+  // is DST on America/Los_Angeles according to current rules.
+  // UTC has no DST part and w/o resetting t mktime will return -1.
+  memset(&t, 0, sizeof(tm));
+  t.tm_year = 2099 - 1900;
+  t.tm_mon = 2;
+  t.tm_mday = 10;
 
   setenv("TZ", "UTC", 1);
   tzset();
   errno = 0;
-  ASSERT_EQ(static_cast<time_t>(4108320000U), mktime(&t));
+  ASSERT_EQ(static_cast<time_t>(4076784000U), mktime(&t));
   ASSERT_EQ(0, errno);
+#endif
+}
+
+TEST(time, mktime_after_2100) {
+   struct tm t;
+   memset(&t, 0, sizeof(tm));
+   t.tm_year = 2150 - 1900;
+   t.tm_mon = 2;
+   t.tm_mday = 10;
+
+#if !defined(__LP64__)
+   // 32-bit bionic has a signed 32-bit time_t.
+   ASSERT_EQ(-1, mktime(&t));
+   ASSERT_EQ(EOVERFLOW, errno);
+#else
+   setenv("TZ", "Europe/London", 1);
+   tzset();
+   errno = 0;
+
+   // Comparing against 1 Mar 2150. Exact value might change
+   // due to government actions or if we bump pregenerated upper bound.
+   ASSERT_LT(static_cast<time_t>(5685379200U), mktime(&t));
+   ASSERT_EQ(0, errno);
+
+   memset(&t, 0, sizeof(tm));
+   t.tm_year = 2150 - 1900;
+   t.tm_mon = 2;
+   t.tm_mday = 10;
+
+   setenv("TZ", "UTC", 1);
+   tzset();
+   errno = 0;
+
+   // Extra transition is created for UTC too, but its offset is 0.
+   ASSERT_EQ(static_cast<time_t>(5686156800U), mktime(&t));
+   ASSERT_EQ(0, errno);
 #endif
 }
 
@@ -177,6 +225,7 @@ TEST(time, mktime_EOVERFLOW) {
   ASSERT_EQ(0, errno);
 
   // This will overflow for LP32 or LP64.
+  memset(&t, 0, sizeof(tm));
   t.tm_year = INT_MAX;
 
   errno = 0;
@@ -204,6 +253,24 @@ TEST(time, strftime) {
   // Date and time as text.
   EXPECT_EQ(24U, strftime(buf, sizeof(buf), "%c", &t));
   EXPECT_STREQ("Sun Mar 10 00:00:00 2100", buf);
+}
+
+TEST(time, strftime_second_before_epoch) {
+  setenv("TZ", "UTC", 1);
+
+  struct tm t;
+  memset(&t, 0, sizeof(tm));
+  t.tm_year = 1969 - 1900;
+  t.tm_mon = 11;
+  t.tm_mday = 31;
+  t.tm_hour = 23;
+  t.tm_min = 59;
+  t.tm_sec = 59;
+
+  char buf[64];
+
+  EXPECT_EQ(2U, strftime(buf, sizeof(buf), "%s", &t));
+  EXPECT_STREQ("-1", buf);
 }
 
 TEST(time, strftime_null_tm_zone) {
