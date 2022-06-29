d2617131a67c882bb2801a717af42f474b539a59
Mao Han <han_mao@linux.alibaba.com>
2022-06-29 15:28:15 +0800

    [RFC]Add riscv64 support
    This patch contains the build infastructure, float point,
    TLS, relocation and ABI related support for rv64.
    
    Signed-off-by: Mao Han <han_mao@linux.alibaba.com>
    Signed-off-by: Xia Lifang <lifang_xia@linux.alibaba.com>
    Signed-off-by: Chen Guoyin <chenguoyin.cgy@linux.alibaba.com>
    Signed-off-by: Wang Chen <wangchen20@iscas.ac.cn>
    Signed-off-by: Lu Xufan <luxufan@iscas.ac.cn>
    Change-Id: Idc573c8203ec07d81fa826e3471614e5eed9df86

--- tests/signal_test.cpp
+++ tests/signal_test.cpp
@@ -823,9 +823,15 @@ TEST(signal, rt_tgsigqueueinfo) {
 #endif
 
 TEST(signal, sigset_size) {
+<<<<<<< PATCH SET (2a5473 [RFC]Add riscv64 support)
+  // The setjmp implementations assume that sigset_t can fit in a
+  // long. This is true because ARM and x86 have broken rt signal support,
+  // and AArch64 and x86_64 and RISCV64 all have a SIGRTMAX defined as 64.
+=======
   // The setjmp implementations assume that sigset_t can fit in a long.
   // This is true because the 32-bit ABIs have broken rt signal support,
   // but the 64-bit ABIs both have a SIGRTMAX defined as 64.
+>>>>>>> BASE      (9874c5 Merge "Nullability check for regex module.")
 #if defined(__BIONIC__)
   static_assert(sizeof(sigset_t) <= sizeof(long), "sigset_t doesn't fit in a long");
 #endif
