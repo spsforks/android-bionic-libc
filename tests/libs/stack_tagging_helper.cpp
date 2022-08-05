82e780eb6f202ba95f959f6724781fa03de767eb
Florian Mayer <fmayer@google.com>
2022-08-05 15:21:07 -0700

    [MTE] test for exception cleanup.
    This depends on the LLVM change https://reviews.llvm.org/D135639.
    
    Test: adb shell /data/local/tmp/stack_tagging_helper exception_cleanup
    Bug: 174878242
    Change-Id: Ia1dfdbe482b40c174acaf6c1ac4ad054470d10b8

--- tests/libs/stack_tagging_helper.cpp
+++ tests/libs/stack_tagging_helper.cpp
@@ -260,6 +260,75 @@ void test_android_mallopt() {
   CHECK(memtag_stack);
 }
 
+static volatile char* throw_frame;
+static volatile char* skip_frame3_frame;
+volatile char *x;
+
+__attribute__((noinline)) void throws() {
+  // Prevent optimization.
+  if (getpid() == 0) return;
+  throw_frame = reinterpret_cast<char*>(__builtin_frame_address(0));
+  throw "error";
+}
+
+__attribute__((noinline)) void maybe_throws() {
+  volatile char y[5 * 16]= {};
+  x = y;
+  // Make sure y is tagged.
+  CHECK((reinterpret_cast<uintptr_t>(&y) & (0xFULL << 56)) !=
+        (reinterpret_cast<uintptr_t>(__builtin_frame_address(0)) & (0xFULL << 56)));
+  throws();
+  memset(const_cast<char*>(&y[0]), 0, sizeof(y));
+}
+
+__attribute__((noinline, no_sanitize("memtag"))) void skip_frame() {
+  volatile char y[6*16] = {};
+  x = y;
+  // Make sure y is not tagged.
+  CHECK((reinterpret_cast<uintptr_t>(&y) & (0xFULL << 56)) ==
+        (reinterpret_cast<uintptr_t>(__builtin_frame_address(0)) & (0xFULL << 56)));
+  maybe_throws();
+  memset(const_cast<char*>(&y[0]), 0, sizeof(y));
+}
+
+__attribute__((noinline)) void skip_frame2() {
+  volatile char y[7*16] = {};
+  x = y;
+  // Make sure y is tagged.
+  CHECK((reinterpret_cast<uintptr_t>(&y) & (0xFULL << 56)) !=
+        (reinterpret_cast<uintptr_t>(__builtin_frame_address(0)) & (0xFULL << 56)));
+  skip_frame();
+  memset(const_cast<char*>(&y[0]), 0, sizeof(y));
+}
+
+__attribute__((noinline, no_sanitize("memtag"))) void skip_frame3() {
+  volatile char y[8*16] = {};
+  x = y;
+  skip_frame3_frame = reinterpret_cast<char*>(__builtin_frame_address(0));
+  skip_frame2();
+  memset(const_cast<char*>(&y[0]), 0, sizeof(y));
+}
+
+void test_exception_cleanup() {
+  volatile char placeholder[16*16] = {};
+  x = placeholder;
+  try {
+    skip_frame3();
+  } catch (const char* e) {
+  }
+  if (throw_frame >= skip_frame3_frame) {
+    fprintf(stderr, "invalid throw frame");
+    exit(1);
+  }
+  for (char* b = const_cast<char*>(throw_frame); b < skip_frame3_frame; ++b) {
+    if (mte_get_tag(b) != b) {
+      fprintf(stderr, "invalid tag at %p", b);
+      exit(1);
+    }
+  }
+  memset(const_cast<char*>(&placeholder[0]), 0, sizeof(placeholder));
+}
+
 int main(int argc, char** argv) {
   if (argc < 2) {
     printf("nothing to do\n");
@@ -293,6 +362,11 @@ int main(int argc, char** argv) {
 
   if (strcmp(argv[1], "android_mallopt") == 0) {
     test_android_mallopt();
+    return 0;
+  }
+
+  if (strcmp(argv[1], "exception_cleanup") == 0) {
+    test_exception_cleanup();
     return 0;
   }
 
