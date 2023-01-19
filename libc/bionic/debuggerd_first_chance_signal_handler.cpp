1bd195cc0df83b6b6137af0f5539546f62871502
Mitch Phillips <mitchp@google.com>
2023-01-19 12:47:22 -0800

    Add the recoverable GWP-ASan feature.
    GWP-ASan's recoverable mode was landed upstream in
    https://reviews.llvm.org/D140173.
    
    This mode allows for a use-after-free or a buffer-overflow bug to be
    detected by GWP-ASan, a crash report dumped, but then GWP-ASan (through
    the preCrashReport() and postCrashReportRecoverableOnly() hooks) will
    patch up the memory so that the process can continue, in spite of the
    memory safety bug.
    
    This is desirable, as it allows us to consider migrating non-system apps
    from opt-in GWP-ASan to opt-out GWP-ASan. The major concern was "if we
    make it opt-out, then bad apps will start crashing". If we don't crash,
    problem solved :). Obviously, we'll need to do this with an amount of
    process sampling to mitigate against the 70KiB memory overhead.
    
    The biggest problem is that the debuggerd signal handler isn't the first
    signal handler for apps, it's the sigchain handler inside of libart.
    Clearly, the sigchain handler needs to ask us whether the crash is
    GWP-ASan's fault, and if so, please patch up the allocator. Because of
    linker namespace restrictions, libart can't directly ask the linker
    (which is where debuggerd lies), so we provide a proxy function in libc.
    
    Test: Build the platform, run sanitizer-status and various test apps
    with recoverable gwp-asan. Assert that it doesn't crash, and we get a
    debuggerd report.
    Bug: 247012630
    
    Change-Id: I2424ad8f881028ade4e13823b110186fecd97cd1

---
+++ libc/bionic/debuggerd_first_chance_signal_handler.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <signal.h>
+
+extern "C" bool __loader_debuggerd_first_chance_signal_handler(int signal_number, siginfo_t* info,
+                                                               void* context);
+
+// This function should be called by the sigchain signal handler in libart,
+// before any additional processing of the signal is done. Due to linker
+// namespace restrictions, libart is not allowed to directly call functions in
+// the loader, so we provide this proxy in libc.
+//
+// This function returns false if the sigchain handler should continue. This
+// function return true if the sigchain handler shold immediately return, which
+// happens when the signal came from GWP-ASan, and we've dumped a debuggerd
+// report and patched up the GWP-ASan allocator to recover from the fault, and
+// regular execution of the program can continue.
+extern "C" bool debuggerd_first_chance_signal_handler(int signal_number, siginfo_t* info,
+                                                      void* context) {
+  return __loader_debuggerd_first_chance_signal_handler(signal_number, info, context);
+}
