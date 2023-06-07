2fb6d0868c2189e6a0158f64695579285369161f
Elliott Hughes <enh@google.com>
2023-06-07 00:29:14 +0000

    Remove __INTRODUCED_IN_ architecture macros.
    No longer used.
    
    Test: treehugger
    Change-Id: I90c2565d80fb65ab9bf3100fb85c897c3ed0afc2

--- tools/versioner/tests/preprocessor/expected/foo.h
+++ tools/versioner/tests/preprocessor/expected/foo.h
@@ -52,32 +52,12 @@ int multiple_archs() __INTRODUCED_IN(14);
 
 #endif
 
-// __INTRODUCED_IN_64(21) should be ignored.
-
-#if (!defined(__LP64__) && __ANDROID_API__ >= 13) || (defined(__LP64__))
-int multiple_introduced_1() __INTRODUCED_IN_ARM(13) __INTRODUCED_IN_X86(13) __INTRODUCED_IN_64(21);
-#endif /* (!defined(__LP64__) && __ANDROID_API__ >= 13) || (defined(__LP64__)) */
-
-
-
-#if (!defined(__LP64__) && __ANDROID_API__ >= 13) || (defined(__LP64__) && __ANDROID_API__ >= 22)
-int multiple_introduced_2() __INTRODUCED_IN_ARM(13) __INTRODUCED_IN_X86(13) __INTRODUCED_IN_64(22);
-#endif /* (!defined(__LP64__) && __ANDROID_API__ >= 13) || (defined(__LP64__) && __ANDROID_API__ >= 22) */
-
-
 // This produces both an LP64 and a not-LP64 check, but it doesn't need to check for all 64-bit
 // targets separately.
 
 #if (!defined(__LP64__) && __ANDROID_API__ >= 23) || (defined(__LP64__) && __ANDROID_API__ >= 23)
-int multiple_introduced_3() __INTRODUCED_IN_32(23) __INTRODUCED_IN_64(23);
+int multiple_introduced() __INTRODUCED_IN_32(23) __INTRODUCED_IN_64(23);
 #endif /* (!defined(__LP64__) && __ANDROID_API__ >= 23) || (defined(__LP64__) && __ANDROID_API__ >= 23) */
-
-
-
-#if (!defined(__LP64__) && __ANDROID_API__ >= 12) || (defined(__LP64__))
-int group_lp32() __INTRODUCED_IN_ARM(12) __INTRODUCED_IN_X86(12);
-#endif /* (!defined(__LP64__) && __ANDROID_API__ >= 12) || (defined(__LP64__)) */
-
 
 #if defined(__cplusplus)
 }
--- tools/versioner/tests/preprocessor/headers/foo.h
+++ tools/versioner/tests/preprocessor/headers/foo.h
@@ -36,16 +36,9 @@ int specific_arch_already_guarded_2() __INTRODUCED_IN(14);
 int multiple_archs() __INTRODUCED_IN(14);
 #endif
 
-// __INTRODUCED_IN_64(21) should be ignored.
-int multiple_introduced_1() __INTRODUCED_IN_ARM(13) __INTRODUCED_IN_X86(13) __INTRODUCED_IN_64(21);
-
-int multiple_introduced_2() __INTRODUCED_IN_ARM(13) __INTRODUCED_IN_X86(13) __INTRODUCED_IN_64(22);
-
 // This produces both an LP64 and a not-LP64 check, but it doesn't need to check for all 64-bit
 // targets separately.
-int multiple_introduced_3() __INTRODUCED_IN_32(23) __INTRODUCED_IN_64(23);
-
-int group_lp32() __INTRODUCED_IN_ARM(12) __INTRODUCED_IN_X86(12);
+int multiple_introduced() __INTRODUCED_IN_32(23) __INTRODUCED_IN_64(23);
 
 #if defined(__cplusplus)
 }
