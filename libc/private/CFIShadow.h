98d3e731268cd882655496efc8e956380aa3e082
Kalesh Singh <kaleshsingh@google.com>
2023-08-17 17:04:21 -0700

    CFIShadow: Convert class to namespace
    libdl does cannot use the STL so it does not have access to
    symbol __cxa_guard_acquire/__cxa_guard_release needed for
    thread safe statics.
    
    All member of CFIShadow class are static, convert this to
    namespace instead to avoid the issue described above.
    
    This is a preparatory change for the next patch in this series.
    
    Bug: 296275298
    Test: m
    Chnage-Id: I577d2fab614df7b9386cf72fc58d3f1feef0267d
    Change-Id: Ib7e2dbb7140868bbf9c31b0063016023fab7f926
    Signed-off-by: Kalesh Singh <kaleshsingh@google.com>

--- libc/private/CFIShadow.h
+++ libc/private/CFIShadow.h
@@ -25,7 +25,7 @@
 constexpr unsigned kLibraryAlignmentBits = 18;
 constexpr size_t kLibraryAlignment = 1UL << kLibraryAlignmentBits;
 
-// This class defines format of the shadow region for Control Flow Integrity support.
+// This namespace defines format of the shadow region for Control Flow Integrity support.
 // See documentation in http://clang.llvm.org/docs/ControlFlowIntegrityDesign.html#shared-library-support.
 //
 // CFI shadow is effectively a very fast and specialized implementation of dladdr: given an address that
@@ -50,42 +50,42 @@ constexpr size_t kLibraryAlignment = 1UL << kLibraryAlignmentBits;
 // most one DSO.
 // Compiler requirement: __cfi_check is aligned at kCfiCheckGranularity.
 // Compiler requirement: __cfi_check for a given DSO is located below any CFI target for that DSO.
-class CFIShadow {
- public:
-  static constexpr uintptr_t kShadowGranularity = kLibraryAlignmentBits;
-  static constexpr uintptr_t kCfiCheckGranularity = 12;
+namespace CFIShadow {
 
-  // Each uint16_t element of the shadow corresponds to this much application memory.
-  static constexpr uintptr_t kShadowAlign = 1UL << kShadowGranularity;
+static constexpr uintptr_t kShadowGranularity = kLibraryAlignmentBits;
+static constexpr uintptr_t kCfiCheckGranularity = 12;
 
-  // Alignment of __cfi_check.
-  static constexpr uintptr_t kCfiCheckAlign = 1UL << kCfiCheckGranularity;  // 4K
+// Each uint16_t element of the shadow corresponds to this much application memory.
+static constexpr uintptr_t kShadowAlign = 1UL << kShadowGranularity;
+
+// Alignment of __cfi_check.
+static constexpr uintptr_t kCfiCheckAlign = 1UL << kCfiCheckGranularity;  // 4K
 
 #if defined (__LP64__)
-  static constexpr uintptr_t kMaxTargetAddr = 0xffffffffffff;
+static constexpr uintptr_t kMaxTargetAddr = 0xffffffffffff;
 #else
-  static constexpr uintptr_t kMaxTargetAddr = 0xffffffff;
+static constexpr uintptr_t kMaxTargetAddr = 0xffffffff;
 #endif
 
-  // Shadow is 2 -> 2**kShadowGranularity.
-  static constexpr uintptr_t kShadowSize =
-      align_up((kMaxTargetAddr >> (kShadowGranularity - 1)), PAGE_SIZE);
+// Shadow is 2 -> 2**kShadowGranularity.
+static constexpr uintptr_t kShadowSize =
+    align_up((kMaxTargetAddr >> (kShadowGranularity - 1)), PAGE_SIZE);
 
-  // Returns offset inside the shadow region for an address.
-  static constexpr uintptr_t MemToShadowOffset(uintptr_t x) {
-    return (x >> kShadowGranularity) << 1;
-  }
+// Returns offset inside the shadow region for an address.
+static constexpr uintptr_t MemToShadowOffset(uintptr_t x) {
+  return (x >> kShadowGranularity) << 1;
+}
 
-  typedef int (*CFICheckFn)(uint64_t, void *, void *);
+typedef int (*CFICheckFn)(uint64_t, void *, void *);
 
- public:
-  enum ShadowValues : uint16_t {
-    kInvalidShadow = 0,    // Not a valid CFI target.
-    kUncheckedShadow = 1,  // Unchecked, valid CFI target.
-    kRegularShadowMin = 2  // This and all higher values encode a negative offset to __cfi_check in
-                           // the units of kCfiCheckGranularity, starting with 0 at
-                           // kRegularShadowMin.
-  };
+enum ShadowValues : uint16_t {
+  kInvalidShadow = 0,    // Not a valid CFI target.
+  kUncheckedShadow = 1,  // Unchecked, valid CFI target.
+  kRegularShadowMin = 2  // This and all higher values encode a negative offset to __cfi_check in
+                         // the units of kCfiCheckGranularity, starting with 0 at
+                         // kRegularShadowMin.
 };
+
+}  // namespace CFIShadow
 
 #endif  // CFI_SHADOW_H
