adf9b6fd5fe6e760fc607983268ce053b18a2f9f
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-08-21 22:29:04 +0000

    Revert "bionic: Fix typo in the argument passed to getauxval()"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/46228b70-2417-4cd8-963a-d9426a0703a5). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I7f8c6950348e29bd9d2aed644b0d57c6df63c9c3

--- libc/platform/bionic/page.h
+++ libc/platform/bionic/page.h
@@ -27,7 +27,7 @@ inline size_t page_size() {
 #if defined(PAGE_SIZE)
   return PAGE_SIZE;
 #else
-  static const size_t page_size = getauxval(AT_PAGESZ);
+  static const size_t page_size = getauxval(PAGE_SIZE);
   return page_size;
 #endif
 }
