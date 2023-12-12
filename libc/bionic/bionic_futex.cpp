0d22fab2b732f52aaa992afc619385f44224bbde
Andy Hung <hunga@google.com>
2023-12-11 19:28:02 -0800

    bionic: fix pthread_mutex_timedlock for PI mutexes
    FUTEX_LOCK_PI uses CLOCK_REALTIME so adjust clock timebase
    accordingly.
    
    FUTEX_WAIT_BITSET, FUTEX_WAIT_REQUEUE_PI, FUTEX_LOCK_PI2
    are updated to check the presence of FUTEX_CLOCK_REALTIME flag.
    
    Test: atest bionic-unit-tests
    Bug: 312787238
    Bug: 315897315
    Change-Id: I2d93286cf22d3b3d9f3757d49b46f7ee9ea5490c

--- libc/bionic/bionic_futex.cpp
+++ libc/bionic/bionic_futex.cpp
@@ -35,7 +35,7 @@
 static inline __always_inline int FutexWithTimeout(volatile void* ftx, int op, int value,
                                                    bool use_realtime_clock,
                                                    const timespec* abs_timeout, int bitset) {
-  const timespec* futex_abs_timeout = abs_timeout;
+  const timespec* _Nonnull futex_abs_timeout = abs_timeout;
   // pthread's and semaphore's default behavior is to use CLOCK_REALTIME, however this behavior is
   // essentially never intended, as that clock is prone to change discontinuously.
   //
@@ -46,13 +46,31 @@ static inline __always_inline int FutexWithTimeout(volatile void* ftx, int op, i
   // We have seen numerous bugs directly attributable to this difference.  Therefore, we provide
   // this general workaround to always use CLOCK_MONOTONIC for waiting, regardless of what the input
   // timespec is.
-  timespec converted_monotonic_abs_timeout;
-  if (abs_timeout && use_realtime_clock) {
-    monotonic_time_from_realtime_time(converted_monotonic_abs_timeout, *abs_timeout);
-    if (converted_monotonic_abs_timeout.tv_sec < 0) {
+  timespec converted_timeout;
+  if (abs_timeout) {
+    // check the clock base.
+    const int opcmd = op & FUTEX_CMD_MASK;
+    const int opcmdrt = op & (FUTEX_CMD_MASK | FUTEX_CLOCK_REALTIME);
+    if (opcmd == FUTEX_WAIT || opcmdrt == FUTEX_WAIT_BITSET || opcmdrt == FUTEX_LOCK_PI2 ||
+        opcmdrt == FUTEX_WAIT_REQUEUE_PI) {
+      // futex op is monotonic, may need to convert abs_timeout.
+      if (use_realtime_clock) {
+        monotonic_time_from_realtime_time(converted_timeout, *abs_timeout);
+        futex_abs_timeout = &converted_timeout;
+      }
+    } else if (opcmd == FUTEX_LOCK_PI || opcmdrt == (FUTEX_WAIT_BITSET | FUTEX_CLOCK_REALTIME) ||
+               opcmdrt == (FUTEX_LOCK_PI2 | FUTEX_CLOCK_REALTIME) ||
+               opcmdrt == (FUTEX_WAIT_REQUEUE_PI | FUTEX_CLOCK_REALTIME)) {
+      // futex op is realtime, may need to convert abs_timeout.
+      if (!use_realtime_clock) {
+        realtime_time_from_monotonic_time(converted_timeout, *abs_timeout);
+        futex_abs_timeout = &converted_timeout;
+      }
+    }
+
+    if (futex_abs_timeout->tv_sec < 0) {
       return -ETIMEDOUT;
     }
-    futex_abs_timeout = &converted_monotonic_abs_timeout;
   }
 
   return __futex(ftx, op, value, futex_abs_timeout, bitset);
@@ -66,6 +84,23 @@ int __futex_wait_ex(volatile void* ftx, bool shared, int value, bool use_realtim
 
 int __futex_pi_lock_ex(volatile void* ftx, bool shared, bool use_realtime_clock,
                        const timespec* abs_timeout) {
-  return FutexWithTimeout(ftx, (shared ? FUTEX_LOCK_PI : FUTEX_LOCK_PI_PRIVATE), 0,
-                          use_realtime_clock, abs_timeout, 0);
+  // We really want FUTEX_LOCK_PI2 which is default CLOCK_MONOTONIC, but that isn't supported
+  // on linux before 5.14.  FUTEX_LOCK_PI uses CLOCK_REALTIME.  Here we verify support.
+
+  static const bool supports_PI2 = [] {
+    uint32_t tmp_ftx_word = 0;
+    if (FutexWithTimeout(&tmp_ftx_word, FUTEX_LOCK_PI2, 0 /* value */,
+                         false /* use_realtime_clock */, nullptr /* abs_timeout */,
+                         0 /* bitset */) == 0) {
+      // tmp_ftx_word contains tid
+      FutexWithTimeout(&tmp_ftx_word, FUTEX_UNLOCK_PI, 0 /* value */,
+                       false /* use_realtime_clock */, nullptr /* abs_timeout */, 0 /* bitset */);
+      return true;
+    }
+    return false;
+  }();
+
+  int op = supports_PI2 ? FUTEX_LOCK_PI2 : FUTEX_LOCK_PI;
+  if (!shared) op |= FUTEX_PRIVATE_FLAG;
+  return FutexWithTimeout(ftx, op, 0 /* value */, use_realtime_clock, abs_timeout, 0 /* bitset */);
 }
