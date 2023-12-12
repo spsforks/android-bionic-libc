b7909863d5719ad2fe9ff2e29996961dd968f17b
Soni, Ravi Kumar <ravi.kumar.soni@intel.com>
2023-12-12 10:55:39 +0530

    Obtain x86 cache info from CPU
    The cache info today is hardcoded in cache.h
    May not be optimal across various uarchs/SKUs
    Leverage cpu_features lib to get the underlying cache.
    
    Improvements seen on TGL, for various sizes
    memmove_non_overlapping
    1.25M - 31%
    1.5M - 30%
    1.75M - 28%
    
    memcpy
    1.25M - 31%
    1.5M - 31%
    1.75M - 30%
    
    Test: bionic/tests/run-on-host.sh 64 && bionic/tests/run-on-host.sh 32
    Bug: 202102347
    Change-Id: I4bbad51794758873744149d0f58b86bb92ee307f
    Signed-off-by: Vinay Prasad Kompella <vinay.kompella@intel.com>
    Signed-off-by: Soni, Ravi Kumar <ravi.kumar.soni@intel.com>

---
+++ libc/private/bionic_cacheinfo.h
@@ -0,0 +1,24 @@
+/*
+Copyright (C) 2023 Intel Corporation.
+
+This software and the related documents are Intel copyrighted materials,
+and your use of them is governed by the express license under which they
+were provided to you (End User License Agreement for the Intel(R) Software
+Development Products). Unless the License provides
+otherwise, you may not use, modify, copy, publish, distribute, disclose or
+transmit this software or the related documents without Intel's prior
+written permission.
+
+This software and the related documents are provided as is, with no
+express or implied warranties, other than those that are expressly
+stated in the License.
+*/
+
+#ifndef _PRIVATE_BIONIC_CPUINFO_H
+#define _PRIVATE_BIONIC_CPUINFO_H
+
+#include <sys/cdefs.h>
+
+__LIBC_HIDDEN__ void __libc_init_cacheinfo();
+
+#endif
