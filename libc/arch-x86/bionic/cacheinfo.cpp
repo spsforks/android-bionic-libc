ecc900536b096bca4e7f608f9fda9118738df743
Soni, Ravi Kumar <ravi.kumar.soni@intel.com>
2023-12-12 10:55:39 +0530

    Obtain x86 cache info from CPU
    The cache info today is hardcoded in cache.h
    May not be optimal across various uarchs/SKUs
    Leverage bionic sysconf to get the underlying cache.
    
    Improvements seen on RPL, for various sizes
    memmove_non_overlapping
    1.25M - 31%
    1.5M - 30%
    1.75M - 28%
    
    memcpy
    1.25M - 31%
    1.5M - 31%
    1.75M - 30%
    
    Test: bionic/tests/run-on-host.sh 64 && bionic/tests/run-on-host.sh 32
    Bug: 202102347
    Change-Id: I4bbad51794758873744149d0f58b86bb92ee307f
    Signed-off-by: Vinay Prasad Kompella <vinay.kompella@intel.com>
    Signed-off-by: Soni, Ravi Kumar <ravi.kumar.soni@intel.com>

---
+++ libc/arch-x86/bionic/cacheinfo.cpp
@@ -0,0 +1,54 @@
+/*
+Copyright (C) 2023 Intel Corporation.
+
+This software and the related documents are Intel copyrighted materials,
+and your use of them is governed by the express license under which they
+were provided to you (End User License Agreement for the Intel(R) Software
+Development Products). Unless the License provides
+otherwise, you may not use, modify, copy, publish, distribute, disclose or
+transmit this software or the related documents without Intel's prior
+written permission.
+
+This software and the related documents are provided as is, with no
+express or implied warranties, other than those that are expressly
+stated in the License.
+*/
+
+#include <include/bits/sysconf.h>
+#include "private/bionic_cacheinfo.h"
+
+#ifdef _PRIVATE_ARCH_CACHE_H_
+/* Data cache size for use in memory and string routines, typically
+   L1 size, rounded to multiple of 256 bytes.  */
+extern unsigned long int __x86_data_cache_size_half;
+extern unsigned long int __x86_data_cache_size;
+/* Shared cache size for use in memory and string routines, typically
+   L2 or L3 size, rounded to multiple of 256 bytes.  */
+extern unsigned long int __x86_shared_cache_size_half;
+extern unsigned long int __x86_shared_cache_size;
+/* Shared cache size for memset routines*/
+extern unsigned long int __x86_shared_cache_size_memset;
+#endif
+
+/* Data cache size for use in memory and string routines, typically
+   L1 size, rounded to multiple of 256 bytes.  */
+unsigned long int __x86_data_cache_size = 24 * 1024;
+unsigned long int __x86_data_cache_size_half = __x86_data_cache_size / 2;
+/* Shared cache size for use in memory and string routines, typically
+   L2 or L3 size, rounded to multiple of 256 bytes.  */
+unsigned long int __x86_shared_cache_size = 1024 * 1024;
+unsigned long int __x86_shared_cache_size_half = __x86_shared_cache_size / 2;
+/* Shared cache size for memset routines*/
+unsigned long int __x86_shared_cache_size_memset = 1024 * 1024;
+
+void __libc_init_cacheinfo() {
+  // L1D
+  __x86_data_cache_size = sysconf(_SC_LEVEL1_DCACHE_SIZE);
+  __x86_data_cache_size_half = __x86_data_cache_size / 2;
+
+  /* Shared cache size */
+  __x86_shared_cache_size = sysconf(_SC_LEVEL2_CACHE_SIZE);
+  __x86_shared_cache_size_half = __x86_shared_cache_size / 2;
+
+  __x86_shared_cache_size_memset = __x86_shared_cache_size * 2;
+}
