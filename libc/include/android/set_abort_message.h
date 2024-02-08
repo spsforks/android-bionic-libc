00d1b2e78e528905deb44e0f416c06a8cb273f6e
Florian Mayer <fmayer@google.com>
2024-02-07 16:25:18 -0800

    Add API to allow apps to attach extra information to tombstones.
    Test: atest debuggerd_test
    Bug: 155462331
    Bug: 309446525
    Change-Id: Idc8387307738957dbba3daaae59f605566329f0f

--- libc/include/android/set_abort_message.h
+++ libc/include/android/set_abort_message.h
@@ -30,12 +30,23 @@
 
 /**
  * @file android/set_abort_message.h
- * @brief The android_set_abort_message() function.
+ * @brief Attach extra information to android crashes.
  */
 
+#include <stddef.h>
+#include <stdint.h>
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
+
+typedef struct crash_detail_t crash_detail_t;
+
+typedef enum : int16_t {
+    // arbitrary bytes that will be hex encoded in the printed tomstone
+    CRASH_DETAIL_BYTES,
+    // human readable string to be printed in the tombstone
+    CRASH_DETAIL_STRING
+} crash_detail_type_t;
 
 /**
  * android_set_abort_message() sets the abort message that will be shown
@@ -45,5 +56,32 @@ __BEGIN_DECLS
  * all FORTIFY/fdsan aborts.
  */
 void android_set_abort_message(const char* _Nullable __msg);
+
+/**
+ * Add a new string that gets logged into a tombstone.
+ * The lifetime of both pointers has to be valid until the program crashes, or until
+ * android_remove_crash_detail is called.
+ * \param name identifying name for this extra data
+ * \param data a buffer containing the extra detail bytes
+ * \param n number of bytes of the buffer pointed to by data
+ * \param type the type of the data referred to by the pointer.
+ *             currently supported are:
+ *               * CRASH_DETAIL_STRING: human readable string to be printed in the tombstone
+ *               * CRASH_DETAIL_BYTES: arbitrary bytes that will be hex encoded in the printed tombstone
+ *
+ * \return a handle to the extra crash detail for use with android_remove_crash_detail.
+ */
+crash_detail_t* _Nullable android_add_crash_detail(
+    const char* _Nonnull name, const char* _Nonnull data, size_t n,
+    crash_detail_type_t type) __INTRODUCED_IN(35);
+
+/**
+ * Remove crash detail from being logged into tombstones.
+ * After this function returns, the lifetime of the objects crash_detail was
+ * constructed from no longer needs to be valid.
+ *
+ * \param crash_detail the crash_detail that should be removed.
+ */
+void android_remove_crash_detail(crash_detail_t* _Nonnull crash_detail) __INTRODUCED_IN(35);
 
 __END_DECLS
