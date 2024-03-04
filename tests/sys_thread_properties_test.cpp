c1c90d27575fe0ed80c0273a0317a12a3b0fb30d
Elliott Hughes <enh@google.com>
2024-03-04 17:07:38 +0000

    Build sys_thread_properties_test.
    Change-Id: If92af30f96351043bbd5dafcbf5c2de8ac274261

--- tests/sys_thread_properties_test.cpp
+++ tests/sys_thread_properties_test.cpp
@@ -20,7 +20,9 @@
 #include "utils.h"
 
 TEST(thread_properties_test, iterate_dts) {
-#if defined(__BIONIC__)
+#if !defined(__BIONIC__)
+  GTEST_SKIP() << "bionic-only API";
+#endif
   const char expected_out[] =
       "got test_static_tls_bounds\niterate_cb i = 0\ndone_iterate_dynamic_tls\n";
   std::string helper = GetTestLibRoot() + "tls_properties_helper/tls_properties_helper";
@@ -29,12 +31,12 @@ TEST(thread_properties_test, iterate_dts) {
   ExecTestHelper eth;
   eth.SetArgs({helper.c_str(), nullptr});
   eth.Run([&]() { execve(helper.c_str(), eth.GetArgs(), eth.GetEnv()); }, 0, expected_out);
-#endif
 }
 
 TEST(thread_properties_test, thread_exit_cb) {
-#if defined(__BIONIC__)
-  // tests/libs/thread_exit_cb_helper.cpp
+#if !defined(__BIONIC__)
+  GTEST_SKIP() << "bionic-only API";
+#endif
   const char expected_out[] = "exit_cb_1 called exit_cb_2 called exit_cb_3 called";
   std::string helper = GetTestLibRoot() + "thread_exit_cb_helper/thread_exit_cb_helper";
   chmod(helper.c_str(), 0755);  // TODO: "x" lost in CTS, b/34945607
@@ -42,6 +44,4 @@ TEST(thread_properties_test, thread_exit_cb) {
   ExecTestHelper eth;
   eth.SetArgs({helper.c_str(), nullptr});
   eth.Run([&]() { execve(helper.c_str(), eth.GetArgs(), eth.GetEnv()); }, 0, expected_out);
-
-#endif
 }
