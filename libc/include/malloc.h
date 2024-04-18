d3cd4882ddc9a7bed7bd0627dea4044e0a9d3c07
Chia-hung Duan <chiahungduan@google.com>
2024-04-17 19:08:48 -0700

    [scudo] -1 is also valid for M_DECAY_TIME
    Test: Run tests
    Change-Id: I044ef84bf0ec97c0f8cb8d3a2340b82218d85efc

--- libc/include/malloc.h
+++ libc/include/malloc.h
@@ -186,7 +186,11 @@ struct mallinfo2 mallinfo2(void) __RENAME(mallinfo);
 int malloc_info(int __must_be_zero, FILE* _Nonnull __fp) __INTRODUCED_IN(23);
 
 /**
- * mallopt() option to set the decay time. Valid values are 0 and 1.
+ * mallopt() option to set the decay time. Valid values are -1, 0 and 1.
+ *   -1 : Disable the releasing of unused pages.
+ *    0 : Release the unused pages immediately.
+ *    1 : Release the unused pages at the interval configured by the allocator
+ *        config.
  *
  * Available since API level 27.
  */
